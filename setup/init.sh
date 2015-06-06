baee9af313e12145d7e225ce64234d5ffb47306a
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-06-06 17:08:05 +0900

    Update init.sh

--- setup/init.sh
+++ setup/init.sh
@@ -1,12 +1,9 @@
 
-
 # Author: jeonghoon.kang@gmail.com
-
-wget -N http://125.7.128.54:8070/wordpress/pub/devel/setup_linux/setup_shell.sh
-wget -N http://125.7.128.54:8070/wordpress/pub/devel/setup_linux/setup_apt.sh
-wget -N http://125.7.128.54:8070/wordpress/pub/devel/setup_linux/setup_code.sh
-#source ./setup_apt.sh
-#source ./setup_shell.sh
-#source ./setup_code.sh
-
-
+export gitdnpath='https://raw.githubusercontent.com/jeonghoonkang/BerePi/master/setup'
+wget -N $gitdnpath/setup_shell.sh
+wget -N $gitdnpath/setup_apt.sh
+wget -N $gitdnpath/setup_code.sh
+source ./setup_apt.sh
+source ./setup_shell.sh
+source ./setup_code.sh
