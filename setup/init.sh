b59361fc12f0aa5dda99d33f54b625bcac1aa278
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-06-06 17:03:22 +0900

    setup script update

--- setup/init.sh
+++ setup/init.sh
@@ -5,8 +5,8 @@
 wget -N http://125.7.128.54:8070/wordpress/pub/devel/setup_linux/setup_shell.sh
 wget -N http://125.7.128.54:8070/wordpress/pub/devel/setup_linux/setup_apt.sh
 wget -N http://125.7.128.54:8070/wordpress/pub/devel/setup_linux/setup_code.sh
-source ./setup_apt.sh
-source ./setup_shell.sh
-source ./setup_code.sh
+#source ./setup_apt.sh
+#source ./setup_shell.sh
+#source ./setup_code.sh
 
 
