d7ade8cdea9308b52b7a4f1a49d0f738db517f92
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-06-07 20:13:19 +0900

    gio update with gpio control with timing print

--- apps/gio/wiringPi/gio_control.c
+++ apps/gio/wiringPi/gio_control.c
@@ -1,19 +1,25 @@
 
 #include <wiringPi.h>
+#include <stdio.h>
+
 int main (void) {
     unsigned int end, start = 0;
+    unsigned int udelaytime = 3000;
     int pin_num = 25;
+
     wiringPiSetup () ;
     pinMode (pin_num, OUTPUT) ;
 
     for (;;) {
         digitalWrite (pin_num, HIGH) ;
         start = micros();
-        delayMicroseconds (50);
+        delayMicroseconds (udelaytime);
         end = micros();
-        printf("u-sec : %d",end-start);
+        printf("start= %d \n",start);
+        printf("endt= %d \n",end);
+        printf("u-sec : %d / target %d usec \n",end-start, udelaytime);
         digitalWrite (pin_num,  LOW) ;
-        delay (50) ;
+        delay (500) ;
     }
     return 0 ;
 }
