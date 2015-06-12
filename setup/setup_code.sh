f3cf967b95ebb366f77ee2640552f86fce93d51c
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-06-12 15:43:01 +0900

    Update setup_code.sh

--- setup/setup_code.sh
+++ setup/setup_code.sh
@@ -4,4 +4,9 @@ cd devel
 svn co http://github.com/jeonghoonkang/BerePi/trunk BerePi  
 git clone git://git.drogon.net/wiringPi wiringPi
 svn co svn://125.7.128.53/danalytics --username=tinyos
+
+mkdir stalk
+cd stalk
+svn co https://github.com/321core/STALK/tree/v0.2k
 cd
+
