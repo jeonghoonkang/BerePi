d1469c5be0b81d4bdde6be04d5ff65b94762c9ad
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-06-12 18:28:58 +0900

    update setup_code.sh to copy start.sh with path

--- setup/setup_code.sh
+++ setup/setup_code.sh
@@ -6,6 +6,8 @@ git clone git://git.drogon.net/wiringPi wiringPi
 svn co svn://125.7.128.53/danalytics --username=tinyos
 
 svn co https://github.com/321core/EnergyManagementSystem/trunk stalk
+cd /home/pi/devel/stalk/code/client
+cp start.sh /home/pi
 cd
 
 # http://125.7.128.54:8070/wordpress/pub/devel/HEMS.apk
