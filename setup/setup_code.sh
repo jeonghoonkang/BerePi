2f4cc9340f9aa892c6099ff98bc25c41b16cc8fe
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-06-12 16:41:51 +0900

    Update setup_code.sh

--- setup/setup_code.sh
+++ setup/setup_code.sh
@@ -5,9 +5,7 @@ svn co http://github.com/jeonghoonkang/BerePi/trunk BerePi
 git clone git://git.drogon.net/wiringPi wiringPi
 svn co svn://125.7.128.53/danalytics --username=tinyos
 
-mkdir stalk
-cd stalk
-svn co https://github.com/321core/EnergyManagementSystem/trunk stalk
+svn co https://github.com/321core/EnergyManagementSystem/trunk 
 cd
 
 # http://125.7.128.54:8070/wordpress/pub/devel/HEMS.apk
