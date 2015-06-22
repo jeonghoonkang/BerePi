1ee4dbe9f7b8ab00545a3d8a8331798f5296f6b1
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-06-22 17:22:43 +0900

    Update setup_apt.sh

--- setup/setup_apt.sh
+++ setup/setup_apt.sh
@@ -1,26 +1,26 @@
 
 #Author : Jeonghoon.Kang (http://github.com/jeonghoonkang)
 
-sudo apt-get -y install lrzsz
-sudo apt-get -y install htop
-sudo apt-get -y install screen
-sudo apt-get -y install lynx
-sudo apt-get -y install subversion
-sudo apt-get -y install ntpdate
-sudo apt-get -y install vim-gnome
-sudo apt-get -y install tree
-sudo apt-get -y install vim
-sudo apt-get -y install dos2unix
+#sudo apt-get -y install lrzsz
+#sudo apt-get -y install htop
+#sudo apt-get -y install screen
+#sudo apt-get -y install lynx
+#sudo apt-get -y install subversion
+#sudo apt-get -y install ntpdate
+#sudo apt-get -y install vim-gnome
+#sudo apt-get -y install tree
+#sudo apt-get -y install vim
+#sudo apt-get -y install dos2unix
 #sudo apt-get -y install dosbox
 
-sudo apt-get -y install python-pip
-sudo apt-get -y install python-matplotlib
-sudo apt-get -y install python-mysqldb
-sudo apt-get -y install python-smbus
-sudo apt-get -y install python-requests
-sudo apt-get -y install python-twisted
-sudo apt-get -y install rsync
-sudo pip install requests --upgrade
-sudo pip install twisted --upgrade
+#sudo apt-get -y install python-pip
+#sudo apt-get -y install python-matplotlib
+#sudo apt-get -y install python-mysqldb
+#sudo apt-get -y install python-smbus
+#sudo apt-get -y install python-requests
+#sudo apt-get -y install python-twisted
+#sudo apt-get -y install rsync
+#sudo pip install requests --upgrade
+#sudo pip install twisted --upgrade
 
 
