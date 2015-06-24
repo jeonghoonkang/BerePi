14482b2621fdb6e4384309d09f43c89eb7fa728b
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-06-24 11:35:53 +0900

    script update

--- setup/init.sh
+++ setup/init.sh
@@ -1,10 +1,23 @@
-
+#!/bin/bash
 # Author: jeonghoon.kang@gmail.com
+
+echo "Help: source init.sh co <-- checkout" 
+echo "      source init.sh up <-- update" 
+
 export gitdnpath='https://raw.githubusercontent.com/jeonghoonkang/BerePi/master/setup'
-wget -N $gitdnpath/setup_shell.sh
-wget -N $gitdnpath/setup_apt.sh
-wget -N $gitdnpath/setup_code.sh
-source ./setup_apt.sh
-source ./setup_shell.sh
-source ./setup_code.sh
+
+if [ $1 = 'co' ]; then
+    echo ".... checking out"
+    wget -N $gitdnpath/setup_shell.sh
+    wget -N $gitdnpath/setup_apt.sh
+    wget -N $gitdnpath/setup_code.sh
+    source ./setup_apt.sh
+    source ./setup_shell.sh
+    source ./setup_code.sh co
+elif [ $1 = 'up' ]; then
+    echo ".... update"
+    ./setup_code.sh up
+fi
+
 unset gitdnpath
+echo ".... ending init.sh"
--- setup/setup_code.sh
+++ setup/setup_code.sh
@@ -1,20 +1,26 @@
-cd
-#mkdir devel
-#cd devel
-#svn co http://github.com/jeonghoonkang/BerePi/trunk BerePi  
-#git clone git://git.drogon.net/wiringPi wiringPi
-#svn co svn://125.7.128.53/danalytics --username=tinyos
+#!/bin/bash
+#Author: jeonghoonkang http://github.com/jeonghoonkang
 
-#svn co https://github.com/321core/EnergyManagementSystem/trunk stalk
-#cd /home/pi/devel/stalk/code/client
-#cp start.sh /home/pi
-#cd
+if [ $1 = 'up' ]; then
+    echo "... updating"
+    cd devel/BerePi/
+    svn up --force
+    cd /home/pi/devel/stalk/
+    svn up --force
+    cd
+else
+    cd
+    mkdir devel
+    cd devel
+    svn co http://github.com/jeonghoonkang/BerePi/trunk BerePi  
+    git clone git://git.drogon.net/wiringPi wiringPi
+    svn co svn://125.7.128.53/danalytics --username=tinyos
+
+    svn co https://github.com/321core/EnergyManagementSystem/trunk stalk
+    cd /home/pi/devel/stalk/code/client
+    cp start.sh /home/pi
+    cd
+fi
 
 # http://125.7.128.54:8070/wordpress/pub/devel/HEMS.apk
 
-cd devel/BerePi/
-svn up --force
-
-cd /home/pi/devel/stalk/
-svn up --force
-cd
