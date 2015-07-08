118c95832fb07358567ca52f4dfbf21dea4e9944
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-07-09 00:15:26 +0900

    Update lcd_connect.py

--- apps/lcd_berepi/lib/lcd_connect.py
+++ apps/lcd_berepi/lib/lcd_connect.py
@@ -23,25 +23,23 @@
 # 18: BLUE LCD Backlight (-)
 
 # Define GPIO to LCD mapping, Raspi pin mapping
-#LCD_RS = 6 # LCD pin 4 : RS (Register Select)
-#LCD_E  = 13 # LCD pin 6 : Enable or Strobe
-#LCD_D4 = 19 # LCD pin 11: Data Bit 4
-#LCD_D5 = 26 # LCD pin 12: Data Bit 5
-#LCD_D6 = 21 # LCD pin 13: Data Bit 6
-#LCD_D7 = 20 # LCD pin 14: Data Bit 7
-#LED_ON = 4
-#LCD_RED = 16 # LCD pin 16: RED LCD Backlight (-)
-#LCD_GREEN = 12 # LCD pin 17: GREEN LCD Backlight (-)
-#LCD_BLUE = 7 # # LCD pin 18: BLUE LCD Backlight (-)
+LCD_RS = 6 # LCD pin 4 : RS (Register Select)
+LCD_E  = 13 # LCD pin 6 : Enable or Strobe
+LCD_D4 = 19 # LCD pin 11: Data Bit 4
+LCD_D5 = 26 # LCD pin 12: Data Bit 5
+LCD_D6 = 21 # LCD pin 13: Data Bit 6
+LCD_D7 = 20 # LCD pin 14: Data Bit 7
+LCD_RED = 16 # LCD pin 16: RED LCD Backlight (-)
+LCD_GREEN = 12 # LCD pin 17: GREEN LCD Backlight (-)
+LCD_BLUE = 7 # # LCD pin 18: BLUE LCD Backlight (-)
 
 # Define GPIO to LCD mapping, Raspi pin mapping
-LCD_RS = 27 # LCD pin 4 : RS (Register Select)
-LCD_E  = 22 # LCD pin 6 : Enable or Strobe
-LCD_D4 = 25 # LCD pin 11: Data Bit 4
-LCD_D5 = 24 # LCD pin 12: Data Bit 5
-LCD_D6 = 23 # LCD pin 13: Data Bit 6
-LCD_D7 = 12 # LCD pin 14: Data Bit 7
-#LED_ON = 4
-LCD_RED = 4 # LCD pin 16: RED LCD Backlight (-)
-LCD_GREEN = 17 # LCD pin 17: GREEN LCD Backlight (-)
-LCD_BLUE = 7 # # LCD pin 18: BLUE LCD Backlight (-)
+#LCD_RS = 27 # LCD pin 4 : RS (Register Select)
+#LCD_E  = 22 # LCD pin 6 : Enable or Strobe
+#LCD_D4 = 25 # LCD pin 11: Data Bit 4
+#LCD_D5 = 24 # LCD pin 12: Data Bit 5
+#LCD_D6 = 23 # LCD pin 13: Data Bit 6
+#LCD_D7 = 12 # LCD pin 14: Data Bit 7
+#LCD_RED = 4 # LCD pin 16: RED LCD Backlight (-)
+#LCD_GREEN = 17 # LCD pin 17: GREEN LCD Backlight (-)
+#LCD_BLUE = 7 # # LCD pin 18: BLUE LCD Backlight (-)
