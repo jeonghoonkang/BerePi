179c81a3e2699fb1ee2c861ea0a2b580780e507b
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-08-26 13:31:29 +0900

    install stalk script name change

--- setup/stalk-install.sh
+++ setup/stalk-install.sh
@@ -1,2 +1,2 @@
-wget https://raw.githubusercontent.com/321core/STALK-INSTALL/master/install-rasberry.sh
-sh install-rasberry.sh
+wget https://raw.githubusercontent.com/321core/STALK-INSTALL/master/install-raspberryPi.sh
+sh install-raspberryPi.sh
