d0b27b5cfa549b1c2a02115fd1f54397396985f8
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-08-28 06:32:16 +0900

    Update stalk-install.sh

--- setup/stalk-install.sh
+++ setup/stalk-install.sh
@@ -1,2 +1,7 @@
+# https://github.com/jeonghoonkang
+# add how to setup the server IP and service 
+
 wget https://raw.githubusercontent.com/321core/STALK-INSTALL/master/install-raspberryPi.sh
 sh install-raspberryPi.sh
+
+ 
