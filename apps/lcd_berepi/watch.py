b38426dd73c58fb8ddbcb2e9c8b6dd3a0af79e01
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-11-16 16:30:57 +0900

    LCD update with CO2 ppm

--- apps/lcd_berepi/watch.py
+++ apps/lcd_berepi/watch.py
@@ -10,11 +10,35 @@ from lcd import *
 sys.path.append("../sht20")
 from sht25class import *
 
+import datetime
+import requests
+import json
+import subprocess
+import argparse
+
+devel_dir="/home/pi/devel"
+tmp_dir=devel_dir+"/danalytics/thingsweb/weblib/recv"
+sys.path.append(tmp_dir)
+from lastvalue import *
+
+def parse_args():
+    parser=argparse.ArgumentParser(description="how to run, watch.py", usage='use "%(prog)s --help" for more information', formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("-ip", "--ipaddress", default="0.0.0.0:0000", help="input ip address of iot db")
+    args = parser.parse_args()
+    return args
 
 def main():
+  arg = parse_args()
+
   # Initialise display
   lcd_init()
   print ip_chk(), wip_chk(), mac_chk(), wmac_chk(), stalk_chk(), time_chk()
+
+  dbip = arg.ipaddress
+  if dbip == "0.0.0.0:0000" :
+      print "  please input ip address of iot DB"
+      print "  It will run without restful get API"
+      dbip = "no_db"
 
   while True:
 
@@ -39,7 +63,7 @@ def main():
     lcd_string('%s WL     ' % (str),LCD_LINE_2,1)
     str = wmac_chk()
     str = str[:-1]
-#   lcd_string('%s' % (str),LCD_LINE_2,1)
+    #lcd_string('%s' % (str),LCD_LINE_2,1)
     blueLCDon()
     time.sleep(1.2) 
         
@@ -50,12 +74,22 @@ def main():
     blueLCDon()
     time.sleep(1) 
 
+    # display time & CO2
+    tstr = time_chk()
+    lcd_string('%s' % (tstr),LCD_LINE_1,1)
+    if dbip != "no_db":
+      co2str = get_last_value(dbip,'gyu_RC1_co2.ppm',{'nodeid':'920'})
+      tmp = round(co2str[0], 2)
+      print "CO2 Level = ", tmp, "ppm"
+      lcd_string('%s ppm' %tmp, LCD_LINE_2,1)
+    whiteLCDon()
+    time.sleep(3) 
+
     lcd_string('%s' % (tstr),LCD_LINE_1,1)
     str = humi_chk()
     lcd_string('%.5s ' % (str),LCD_LINE_2,1)
     whiteLCDon()
     time.sleep(2) 
-
 
 def run_cmd(cmd):
     p = Popen(cmd, shell=True, stdout=PIPE)
@@ -105,6 +139,7 @@ if __name__ == '__main__':
   except KeyboardInterrupt:
     pass
   finally:
+    lcd_init()
     lcd_byte(0x01, LCD_CMD)
     lcd_string("Goodbye!",LCD_LINE_1,2)
     GPIO.cleanup()
