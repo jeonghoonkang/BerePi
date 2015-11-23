6042d5c8d0b605f3af34f7e2d618a27ebd691b24
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-11-24 04:15:19 +0900

    Update cycling_light.py

--- apps/hue/cycling_light.py
+++ apps/hue/cycling_light.py
@@ -20,20 +20,31 @@ def shifthue() :
     try:
         conn.request("PUT",callurl ,'{"on":false}')
         response = conn.getresponse()
+        data = response.read()
     except:
-        print "keep going ..."
-        
-    data = response.read()
+        print "keep goging...."
+        time.sleep(2)
+
     time.sleep(1)
     for num in [3,2,1,4] :
         callurl = restcmd + "/%s/state"%(num)
         print callurl
         huenumber = (xhue[4-num])
-        conn.request("PUT",callurl ,'{"on":true, "sat":254, "bri":254, "hue":%s}'%huenumber)
-        response = conn.getresponse()
-        data = response.read()
-        print data
-        time.sleep(1)
+        try :
+            conn.request("PUT",callurl ,'{"on":false}')
+            response = conn.getresponse()
+            data = response.read()
+            time.sleep(1)
+
+            conn.request("PUT",callurl ,'{"on":true, "sat":254, "bri":254, "hue":%s}'%huenumber)
+            response = conn.getresponse()
+            data = response.read()
+            print data
+            time.sleep(1)
+        except:
+            print "exception conn.getresponse from Hue GW"
+            time.sleep(2)
+
 
         
 if __name__ == "__main__": 
