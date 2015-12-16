f49433ea6e9740edbae41e1e488a135709439d57
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-12-16 11:27:24 +0900

    update

--- apps/email/email.py
+++ apps/email/email.py
@@ -1,9 +1,14 @@
+import requests
 # Try running this locally.
-def send_simple_message():
-    return requests.post(
-        "https://api.mailgun.net/v3/samples.mailgun.org/messages",
-        auth=("api", "key-3ax6xnjp29jd6fds4gc373sgvjxteol0"),
-        data={"from": "Excited User <excited@samples.mailgun.org>",
-              "to": ["devs@mailgun.net"],
-              "subject": "Hello",
-              "text": "Testing some Mailgun awesomeness!"})
+def send_email():
+  return requests.post(
+    "https://api.mailgun.net/v3/samples.mailgun.org/messages",
+
+    auth=("api", "key-3ax6xnjp29jd6fds4gc373sgvjxteol0"),
+    data={"from": "Excited User <excited@samples.mailgun.org>",
+    "to": ["devs@mailgun.net"],
+    "subject": "Hello",
+    "text": "Testing some Mailgun awesomeness!"})
+
+if __name__ == "__main__" :
+    send_email()
