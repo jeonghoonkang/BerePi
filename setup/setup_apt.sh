e74076ada4d1d248fc929a1ca8c68a241e6e9646
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-12-18 02:24:08 +0900

    setup apt shell

--- setup/setup_apt.sh
+++ setup/setup_apt.sh
@@ -21,12 +21,15 @@
 #sudo apt-get -y install python-mysqldb
 #sudo apt-get -y install python-smbus
 #sudo apt-get -y install python-requests
-sudo apt-get -y install python-dev
+#sudo apt-get -y install python-dev
 #sudo apt-get -y install python-twisted
 #sudo pip install requests --upgrade
 #sudo pip install twisted --upgrade
 #sudo pip install numpy
 #sudo pip install networkx
+#sudo pip install httplib
+#sudo pip install urllib3
+sudo pip install utils
 
 #########################################################################
 #### for the Cygwin, windows
