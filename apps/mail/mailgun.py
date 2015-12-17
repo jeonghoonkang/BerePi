12eb8ac2b6d28b1337831837c6216432dcbd5421
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-12-17 11:51:25 +0900

    mailgun is working ! Ye~~~

--- apps/mail/mailgun.py
+++ apps/mail/mailgun.py
@@ -2,7 +2,7 @@ import requests
 # Try running this locally.
 def send_email():
   return requests.post(
-    "https://api.mailgun.net/v3/sandboxc1aa4e92f05144a3973f6f8c031849e3.mailgun.org",
+    "https://api.mailgun.net/v3/sandboxc1aa4e92f05144a3973f6f8c031849e3.mailgun.org/messages",
     auth=("api", "key-3fb9acb996e674af4816edf1055a72f7"),
     data={"from": "TinyOS DEV <kang@keti>",
     "to": ["gadin.kang@gmail.com"],
