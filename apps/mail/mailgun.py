cee33ab9687e242f43abcdfde1a1e99361eb9566
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-12-19 03:29:16 +0900

    Update mailgun.py

--- apps/mail/mailgun.py
+++ apps/mail/mailgun.py
@@ -4,10 +4,10 @@ def send_email():
   return requests.post(
     "https://api.mailgun.net/v3/sandboxa4e92f05144a3973f6f8c031849e3.mailgun.org/messages",
     auth=("api", "key-3fb9acb996e674af4816edf1055a7"),
-    data={"from": "TinyOS DEV <kang@keti>",
+    data={"from": "BerePi/TinyOS<sensormail@sky>",
     "to": ["gadin.kang@gmail.com"],
-    "subject": "Sensor mail running",
-    "text": "Merry Christmas !"})
+    "subject": "[SensorMail] Sensor mail running",
+    "text": "Know Yourself !"})
 
 if __name__ == "__main__" :
     send_email()
