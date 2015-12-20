0e1649b4c8739209c7be61c228b9b0dcd2ed7233
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-12-20 16:21:22 +0900

    add startup_sw.sh

---
+++ startup_sw.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+alias sr='screen'
+sudo python /home/pi/devel/BerePi/apps/lcd_berepi/watch.py -ip xxxxx
+#sudo screen -dmS lcd sudo python /home/pi/devel/BerePi/apps/lcd_berepi/watch.py -ip xxxxx
+
+cd
+
