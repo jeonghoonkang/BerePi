793a2b96aa12e2d777b94c93866e321fc617de69
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2015-12-31 05:42:09 +0900

    Update startup_sw.sh

--- setup/startup_sw.sh
+++ setup/startup_sw.sh
@@ -2,4 +2,5 @@
 # print booted mesage on LCD
 # need connected BerePi LCD module hardware
 
-sudo screen -dmS booted_lcd sudo python /home/pi/devel/BerePi/apps/booted/booted_lcd.py 
+#sudo screen -dmS booted_lcd 
+python /home/pi/devel/BerePi/apps/booted/booted_lcd.py 
