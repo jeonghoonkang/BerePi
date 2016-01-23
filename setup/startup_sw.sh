15305543136e2940f0ae8f5a90b6981f744c331b
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2016-01-23 14:16:50 +0900

    startup modified

--- setup/startup_sw.sh
+++ setup/startup_sw.sh
@@ -1,6 +1,8 @@
 
-# print booted mesage on LCD
-# need connected BerePi LCD module hardware
-
-#sudo screen -dmS booted_lcd 
-python /home/pi/devel/BerePi/apps/booted/booted_lcd.py 
+export tmppath=/home/pi/devel/BerePi/apps/lcd_berepi
+cd $tmppath
+pwd
+echo "[BerePi] starting watch app"
+screen -dmS lcddisp sudo python watch.py -ip x.x.x.53:4242
+unset tmppath
+cd
