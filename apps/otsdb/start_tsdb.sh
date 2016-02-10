ca96b69f01a707c33d61fe8dc1e9901702aecc7a
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2016-02-10 17:53:11 +0900

    openTSDB auto-start complete

--- apps/otsdb/start_tsdb.sh
+++ apps/otsdb/start_tsdb.sh
@@ -5,12 +5,12 @@ echo "[BerePi] starting openTSDB by BerePi start-up script"
 
 cd /usr/local/hadoop/hbase-1.0.1.1/
 #sudo sh ./bin/stop-hbase.sh
-sudo sh ./bin/start-hbase.sh && sleep 25
+sudo sh ./bin/start-hbase.sh && sleep 90
 
 cd /usr/local/opentsdb
 sudo screen -dmS tsd_start sudo sh /usr/local/opentsdb/build/tsdb tsd --port=4242 --staticroot=/usr/local/opentsdb/build/staticroot --cachedir=/usr/local/opentsdb/tmp --auto-metric
 
-sleep 3
+sleep 10
 
 cd /usr/local/tcollector
 sudo ./startstop start
