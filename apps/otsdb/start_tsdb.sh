ffece53fed7721e7c8a885acbdff5b4dca9e6730
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2016-02-10 14:55:42 +0900

    Update start_tsdb.sh

--- apps/otsdb/start_tsdb.sh
+++ apps/otsdb/start_tsdb.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
 sudo cd /usr/local/hadoop/hbase-1.0.1.1/
-#sudo ./stop-hbase.sh
-sudo ./start-hbase.sh
+#sudo ./bin/stop-hbase.sh
+sudo ./bin/start-hbase.sh
 
 sudo cd /usr/local/opentsdb
 sudo sh /usr/local/opentsdb/build/tsdb tsd --port=4242 --staticroot=/usr/local/opentsdb/build/staticroot --cachedir=/usr/local/opentsdb/tmp --auto-metric 
