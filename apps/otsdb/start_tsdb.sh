197c7837ddcb536066a2c57c753143aba251a982
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2016-02-10 14:52:12 +0900

    tsdb start up script

--- apps/otsdb/start_tsdb.sh
+++ apps/otsdb/start_tsdb.sh
@@ -1,17 +1,12 @@
 #/bin/bash
 
-cd /usr/local/hadoop/hbase-1.1.3/bin/
-sudo ./stop-hbase.sh
+sudo cd /usr/local/hadoop/hbase-1.0.1.1/
+#sudo ./stop-hbase.sh
 sudo ./start-hbase.sh
 
-cd /usr/local/opentsdb
-export JAVA_HOME=/usr 
-export COMPRESSION="NONE" 
-export HBASE_HOME="/usr/local/hadoop/hbase-1.1.3"
-sudo ./src/create_table.sh
-export tsdtmp=${TMPDIR-'/usr/local/opentsdb/tmp'}/tsd 
-sudo sh /usr/local/opentsdb/build/tsdb tsd --port=4242 --staticroot=/usr/local/opentsdb/build/staticroot --cachedir="/usr/local/opentsdb/tmp" --auto-metric --zkquorum=iot.iptime.org:2181
+sudo cd /usr/local/opentsdb
+sudo sh /usr/local/opentsdb/build/tsdb tsd --port=4242 --staticroot=/usr/local/opentsdb/build/staticroot --cachedir=/usr/local/opentsdb/tmp --auto-metric 
 
-cd /usr/local/tcollector
+sudo cd /usr/local/tcollector
 sudo ./startstop start
 
