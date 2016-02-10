1c079a40bdb87e158df0d8514a06040e1b61b3eb
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2016-02-10 15:42:07 +0900

    startup

--- apps/otsdb/start_tsdb.sh
+++ apps/otsdb/start_tsdb.sh
@@ -1,16 +1,16 @@
 #/bin/bash
-
+echo "----------------------------------------------------"
 echo "[BerePi] starting openTSDB by BerePi start-up script"
 
 cd /usr/local/hadoop/hbase-1.0.1.1/
 #sudo sh ./bin/stop-hbase.sh
 sudo sh ./bin/start-hbase.sh
 
-
 cd /usr/local/opentsdb
-screen -dmS tsdb_start sudo sh /usr/local/opentsdb/build/tsdb tsd --port=4242 --staticroot=/usr/local/opentsdb/build/staticroot --cachedir=/usr/local/opentsdb/tmp --auto-metric
+sudo sh /usr/local/opentsdb/build/tsdb tsd --port=4242 --staticroot=/usr/local/opentsdb/build/staticroot --cachedir=/usr/local/opentsdb/tmp --auto-metric &
 
 cd /usr/local/tcollector
 sudo ./startstop start
 
 echo "[BerePi] last step of run openTSDB, tcollector by BerePi start-up script"
+echo "------------------------------------------------------------------------"
