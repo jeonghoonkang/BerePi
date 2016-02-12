eded1b5642ab29b67ee64bd9ae83f4a9c6c55eb6
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2016-02-13 00:52:04 +0900

    Update start_tsdb.sh

--- apps/otsdb/start_tsdb.sh
+++ apps/otsdb/start_tsdb.sh
@@ -5,7 +5,7 @@ echo "[BerePi] starting openTSDB by BerePi start-up script"
 
 cd /usr/local/hadoop/hbase-1.0.1.1/
 #sudo sh ./bin/stop-hbase.sh
-sudo sh ./bin/start-hbase.sh && sleep 90
+sudo sh ./bin/start-hbase.sh && sleep 90 # raspberryPi zero 에서는 90초 정도 후 Hbase 시작
 
 cd /usr/local/opentsdb
 sudo screen -dmS tsd_start sudo sh /usr/local/opentsdb/build/tsdb tsd --port=4242 --staticroot=/usr/local/opentsdb/build/staticroot --cachedir=/usr/local/opentsdb/tmp --auto-metric
