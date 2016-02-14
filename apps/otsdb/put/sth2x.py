5dce4fc76902f6a2afed6b9b8234bc3082517567
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2016-02-15 05:44:16 +0900

    otsdb sth2x.py

--- apps/otsdb/put/sth2x.py
+++ apps/otsdb/put/sth2x.py
@@ -7,28 +7,29 @@ tmp_dir=devel_dir+"/BerePi/apps"
 from types import *
 import sys
 from time import strftime, localtime
-sys.path.append(tmp_dir+"/sht20")
-from sht25class import *
-
 import datetime
 import requests
 import json
 import subprocess
 
+sys.path.append(tmp_dir+"/sht20")
+from sht25class import *
+
 ################
-url = "http://xxx.xxx.xxx.xxx/api/put"
+url = "http://10.0.0.43:4242/api/put"
 
 def otsdb_restful_put():
     sht21_temp = temp_chk()
     temp_v=str(sht21_temp)
     if temp_v != "-100" :
-    	print('Here %-5.2f `C' % (sht21_temp))
+    	print('otsdb %-5.2f `C' % (sht21_temp))
     ret = humi_chk()
     retstr = str(ret)
-        if retstr != "-100" :
-        print('Here %-5.2f %%' % (ret))
-    hostname = hostname_chk()
+    if retstr != "-100" :
+        print('otsdb %-5.2f %%' % (ret))
 
+    #sname = hostname_chk()
+    sname = "kang-odesk-03"
     data = {
         "metric": "z_beta.temp.sht2x.dgree",
         "timestamp": time.time(),
@@ -37,21 +38,25 @@ def otsdb_restful_put():
             #"eth0": macAddr,
             #"stalk": "VOLOSSH" ,
             "sensor" : "sht2x",
-	    "name" : hostname,
-	    "floor_room": "6F_office",
-	    "building": "global_rnd_center",
-	    "owner": "kang",
-	    "country": "kor"
-	}
+	        "name" : sname,
+	        "floor_room": "6F_office",
+	        "building": "global_rnd_center",
+	        "owner": "kang",
+	        "country": "kor"
+        }
 	#tags should be less than 9, 8 is alright, 9 returns http error
-	try :
-		ret = requests.post(url, data=json.dumps(data))
-	except requests.exceptions.Timeout :
-		logger.error("http connection error, Timeout  %s", ret)
-		continue
-	except requests.exceptions.ConnectionError :
-		logger.error("http connection error, Too many requests %s")
-		continue
+    }
+
+    try :
+        ret = requests.post(url, data=json.dumps(data))
+    except requests.exceptions.Timeout :
+        logger.error("http connection error, Timeout  %s", ret)
+        pass 
+    except requests.exceptions.ConnectionError :
+        logger.error("http connection error, Too many requests %s")
+        pass
+
+    return
 
 def temp_chk():
     temperature = getTemperature()
@@ -61,12 +66,13 @@ def humi_chk():
     humidity = getHumidity()
     return humidity
 
+def hostname_chk():
+    cmd = "hostname"
+    return run_cmd(cmd)
+
 def run_cmd(cmd):
     p = Popen(cmd, shell=True, stdout=PIPE)
     output = p.communicate()[0]
     return output
 
-def hostname_chk():
-    cmd = "hostname"
-    return run_cmd(cmd)
     
