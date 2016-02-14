625c510f08aa94c08ee3ac620dd0d17ee722ed65
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2016-02-15 05:19:59 +0900

    Update sth2x.py

--- apps/otsdb/put/sth2x.py
+++ apps/otsdb/put/sth2x.py
@@ -14,21 +14,44 @@ import datetime
 import requests
 import json
 import subprocess
-import argparse
 
 ################
+url = "http://xxx.xxx.xxx.xxx/api/put"
 
-sht21_temp = temp_chk()
-temp_v=str(sht21_temp)
-if temp_v != "-100" :
-    print('Here %-5.2f `C' % (sht21_temp))
-    
-ret = humi_chk()
-retstr = str(ret)
-if retstr != "-100" :
-    print('Here %-5.2f %%' % (ret))
+def otsdb_restful_put():
+    sht21_temp = temp_chk()
+    temp_v=str(sht21_temp)
+    if temp_v != "-100" :
+    	print('Here %-5.2f `C' % (sht21_temp))
+    ret = humi_chk()
+    retstr = str(ret)
+        if retstr != "-100" :
+        print('Here %-5.2f %%' % (ret))
+    hostname = hostname_chk()
 
-hostname = hostname_chk()
+    data = {
+        "metric": "z_beta.temp.sht2x.dgree",
+        "timestamp": time.time(),
+        "value": sht21_temp, #integer
+        "tags": {
+            #"eth0": macAddr,
+            #"stalk": "VOLOSSH" ,
+            "sensor" : "sht2x",
+	    "name" : hostname,
+	    "floor_room": "6F_office",
+	    "building": "global_rnd_center",
+	    "owner": "kang",
+	    "country": "kor"
+	}
+	#tags should be less than 9, 8 is alright, 9 returns http error
+	try :
+		ret = requests.post(url, data=json.dumps(data))
+	except requests.exceptions.Timeout :
+		logger.error("http connection error, Timeout  %s", ret)
+		continue
+	except requests.exceptions.ConnectionError :
+		logger.error("http connection error, Too many requests %s")
+		continue
 
 def temp_chk():
     temperature = getTemperature()
@@ -37,35 +60,6 @@ def temp_chk():
 def humi_chk():
     humidity = getHumidity()
     return humidity
-
-url = "http://xxx.xxx.xxx.xxx/api/put"
-
-data = {
-    "metric": "z_beta.temp.sht2x.dgree",
-    "timestamp": time.time(),
-    "value": sht21_temp, #integer
-    "tags": {
-        #"eth0": macAddr,
-        #"stalk": "VOLOSSH" ,
-        "sensor" : "sht2x",
-		"name" : hostname,
-		"floor_room": "6F_office",
-		"building": "global_rnd_center",
-		"owner": "kang",
-		"country": "kor"
-	}
-	#tags should be less than 9, 8 is alright, 9 returns http error
-
-	try :
-		ret = requests.post(url, data=json.dumps(data))
-	
-	except requests.exceptions.Timeout :
-		logger.error("http connection error, Timeout  %s", ret)
-		continue
-
-	except requests.exceptions.ConnectionError :
-		logger.error("http connection error, Too many requests %s")
-		continue
 
 def run_cmd(cmd):
     p = Popen(cmd, shell=True, stdout=PIPE)
