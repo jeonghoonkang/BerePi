e06232eec0e536c3eae1ac5c950ca13d5cdbec45
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2016-03-07 07:11:03 +0900

    Update publicip.py

--- apps/tinyosGW/publicip.py
+++ apps/tinyosGW/publicip.py
@@ -23,14 +23,14 @@ if __name__ == '__main__':
     cmd = "cd /home/tinyos/my_deamon/output"
     print run_cmd(cmd)
     #cmd = "touch /home/tinyos/my_deamon/output"
-    cmd_100 = "ssh pi@iot.iptime.org "
+    cmd_100 = "ssh pi@iptime.org "
     cmd_010 = "cd my_deamon/output && echo %s is Lab Server Room IP " %ip[:-1]
     cmd_001 = " | cat > ip.html" 
     cmd = cmd_010 + cmd_001  
     print run_cmd(cmd)
 
-    cmd_200 = "scp pi@iot.iptime.org "
-    cmd = "scp" + " ip.html" + " pi@iot.iptime.org:" + "www/cog" 
+    cmd_200 = "scp pi@iptime.org "
+    cmd = "scp" + " ip.html" + " pi@iptime.org:" + "www/cog" 
     print cmd
     print run_cmd(cmd)
 
