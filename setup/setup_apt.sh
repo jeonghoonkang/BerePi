c5e240854666ceebc941840668c7f60775ebfcb5
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2016-03-07 01:14:35 +0900

    Update setup_apt.sh

--- setup/setup_apt.sh
+++ setup/setup_apt.sh
@@ -24,6 +24,7 @@
 #sudo apt-get -y install samba samba-common-bin
 #sudo apt-get -y install php5 libapache2-mod-php5 
 #sudo apt-get -y install mysql-server php5-mysql
+#sudo apt-get -y install postfix
 
 # test cygwin
 #wget https://pypi.python.org/packages/source/d/distribute/distribute-0.7.3.zip
