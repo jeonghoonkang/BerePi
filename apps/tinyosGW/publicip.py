8b197c70d30f3ce7fb92585f2ccb4589fee8721e
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2016-03-06 20:41:02 +0900

    Create publicip.py

---
+++ apps/tinyosGW/publicip.py
@@ -0,0 +1,10 @@
+
+def run_cmd(cmd):
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate()[0]
+    return output
+
+def mac_chk():
+    cmd = "ifconfig -a | grep ^eth | awk '{print $5}'"
+    macAddr = run_cmd(cmd)
+    return macAddr
