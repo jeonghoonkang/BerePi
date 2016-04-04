19ca267ca7e7795bbb3267d9e42264a4af1ca621
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2016-04-05 00:03:08 +0900

    Update zwave.py

--- apps/zwave/zwave.py
+++ apps/zwave/zwave.py
@@ -2,5 +2,6 @@
 # Author : Jeonghoonkang, github.com/jeonghoonkang
 
 # using ozcp daemon for contorl and monitoring
-# ozwc with Restful API, requests package
+# ozwc with Restful API, requests package, middleware
+
 
