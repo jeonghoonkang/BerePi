55e4b7f32204fe29a542f64aa43b0aab43100494
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2016-08-28 11:23:11 +0900

    Update start_opentsdb.sh

--- setup/opentsdb/start_opentsdb.sh
+++ setup/opentsdb/start_opentsdb.sh
@@ -4,6 +4,7 @@
 # this shell file should run by root "sudo -s"
 # please check the path of openTSDB
 
+
 /usr./build/tsdb tsdb --port=4242 --staticroot=/usr/local/opentsdb/build/staticroot --cachedir=/usr/local/data --auto-metric
 
 
