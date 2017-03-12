7ea63efe13cf562887668b5cd6f428adc1b8a36e
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2017-03-13 00:21:55 +0900

    Update setup_apt.sh

--- setup/setup_apt.sh
+++ setup/setup_apt.sh
@@ -1,15 +1,17 @@
 
 #Author : Jeonghoon.Kang (http://github.com/jeonghoonkang)
 
+#sudo apt-get -y install subversion
+#sudo apt-get -y install dosbox
 #sudo apt-get update && sudo apt-get upgrade -y
 #sudo apt-get update && sudo apt-get dist-upgrade
 #sudo apt-get install apt-transport-https -y --force-yes
 #sudo apt-get update && sudo apt-get install oracle-java7-jdk
+#sudo apt-get -y install wicd wicd-curses
 
 sudo apt-get -y install htop
 sudo apt-get -y install screen
 sudo apt-get -y install rsync
-#sudo apt-get -y install subversion
 sudo apt-get -y install vim
 sudo apt-get -y install tree
 sudo apt-get -y install nmap
@@ -17,19 +19,18 @@ sudo apt-get -y install unzip
 sudo apt-get -y install dnsutils
 sudo apt-get -y install curl
 sudo apt-get -y install dcfldd pv
-
 sudo apt-get -y install lrzsz
 sudo apt-get -y install lynx
 sudo apt-get -y install ntpdate
 sudo apt-get -y install vim-gnome
 sudo apt-get -y install dos2unix
-#sudo apt-get -y install dosbox
-sudo apt-get -y install wicd wicd-curses
 sudo apt-get -y install samba samba-common-bin
 sudo apt-get -y install php5 libapache2-mod-php5 
 sudo apt-get -y install mysql-server php5-mysql
 sudo apt-get -y install postfix
 sudo apt-get -y install rrdtool
+
+
 
 # test cygwin
 wget https://pypi.python.org/packages/source/d/distribute/distribute-0.7.3.zip
@@ -37,6 +38,11 @@ unzip distribute-0.7.3.zip
 cd distribute-0.7.3
 sudo python setup.py install
 sudo easy_install pip
+
+#for cygwin
+#apt-cyg -y install unzip
+#python setup.py install
+#easy_install pip
 
 sudo apt-get -y install python-matplotlib
 sudo apt-get -y install python-mysqldb
