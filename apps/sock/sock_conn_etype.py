ea967798c6d09777e683a90f873b22c453b3183e
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2017-03-16 15:19:19 +0900

    sock

--- apps/sock/sock_conn_etype.py
+++ apps/sock/sock_conn_etype.py
@@ -4,7 +4,7 @@
 # Author : jeonghoonkang, https://github.com/jeonghoonkang
 
 
-import socket
+from socket import *
 import os
 import sys
 import struct
@@ -12,26 +12,39 @@ import time
 
 ETYPE_VALUE_MAX = 8000
 
+
 def main():
-	SERVER_ADDR = "127.0.0.1"
-	SERVER_PORT = 4000
-	print "   try connecting .... "
 
-	sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-	ret_conn = sock.connect( ( SERVER_ADDR, SERVER_PORT ) )
+    splug_acc_flag = 0
+    etype_acc_flag = 0
 
-	readBuffer = ""
-	receivedLines = []
-	sendLines = []
+    #SERVER_ADDR = "222.239.78.8"
+    #SERVER_PORT = 8283   # 40001 samho  9293 hana
+    HOST='0.0.0.0'
+    LOCAL_PORT = 4000
 
-	while True:
-		# provide buffer
-		recv = sock.recv( 1024 )
-		readBuffer += recv
+    sock = socket(AF_INET,SOCK_STREAM )
+    ##for client
+    #sock.connect( ( SERVER_ADDR, SERVER_PORT ) )
+    ret = sock.bind((HOST,LOCAL_PORT))
+    print ret
 
-        print " "*20, "Packet comming"
-		print recv
+    sock.listen(100)
 
+    conn, addr = sock.accpet()
+
+
+    print " "*10, "connected by sock client", addr
+
+
+    readBuffer = ""
+    receivedLines = []
+    sendLines = []
+    while True:
+    # provide buffer
+        recv = conn.recv( 1024 )
+        readBuffer += recv
+        print readBuffer
 
 if __name__ == "__main__":
     main()
