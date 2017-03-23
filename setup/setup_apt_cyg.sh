5a4b035a3e462f6ef437aa1bb7d8c5b585520fe8
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2017-03-24 01:16:45 +0900

    Create setup_apt_cyg.sh

---
+++ setup/setup_apt_cyg.sh
@@ -0,0 +1,40 @@
+
+#gcc installation, try
+apt-cyg install gcc-core
+apt-cyg install cygwin32-freetype2
+apt-cyg install pkg-config
+apt-cyg install libX11-devel
+apt-cyg install make
+apt-cyg install cmake
+apt-cyg install xinit
+apt-cyg install libQt5Core-devel
+apt-cyg install python-devel
+apt-cyg install gcc-g++ libzmq-devel libzmq5
+
+#instead of pip install numpy
+apt-cyg install python-numpy
+apt-cyg install libfreetype-devel
+
+#pip installation
+# wget https://raw.githubusercontent.com/pypa/pip/master/contrib/get-pip.py
+# python get-pip.py
+
+# numpy installation takes a few minutes
+# pip install matplotlib
+# pip install scimath
+# pip install networkx
+
+#########################################################################
+#### some commands
+
+# tasklist
+# ps -W
+
+#### python pip in cygwin
+#http://lelandbatey.com/posts/2014/08/installing-pip-in-cygwin
+
+### FYI, Serial and pip in Cygwin
+# apt-cyg install python-setuptools
+#easy_install-2.7 pip
+# pip install pyserial
+# ( https://github.com/pyserial/pyserial/ )
