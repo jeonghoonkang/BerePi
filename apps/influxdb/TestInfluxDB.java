411969086a7cc1f9399cc8dc1feca7a72f61a95b
sukunkim <sukun.kim@gmail.com>
2017-05-24 08:58:17 +0900

    Added author information and cleaned up code

--- apps/influxdb/TestInfluxDB.java
+++ apps/influxdb/TestInfluxDB.java
@@ -1,10 +1,17 @@
-
-// Author : sukunkim, github.com/sukunkim
+/**
+ * File: TestInfluxDB.java
+ *
+ * Description:
+ * TestInfluxDB class tests InfluxDB in Java.
+ *
+ * @author Sukun Kim <sukunkim@sinbinet.com>
+ */
 
 import java.util.concurrent.TimeUnit;
 
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.BatchPoints;
 import org.influxdb.dto.Point;
 import org.influxdb.dto.Pong;
 import org.influxdb.dto.Query;
@@ -13,14 +20,17 @@ import org.influxdb.dto.QueryResult;
 
 public class TestInfluxDB {
 
-  private InfluxDB influxDB;
-  private String dbName;
+  public static final String HOST = "192.168.149.146";
+  public static final int PORT = 8086;
+  public static final String ID = "root";
+  public static final String PW = "root";
+  public static final String DB = "test";
 
 
   public TestInfluxDB() {
-    influxDB = InfluxDBFactory.connect(
-      "http://192.168.149.146:8086", "root", "root");
-    dbName = "pobmons";
+    InfluxDB influxDB
+      = InfluxDBFactory.connect("http://" + HOST + ":" + PORT, ID, PW);
+
 
     Pong pong = influxDB.ping();
     System.out.println("pong = " + pong);
@@ -28,27 +38,47 @@ public class TestInfluxDB {
     String version = influxDB.version();
     System.out.println("version = " + version);
 
-    influxDB.enableBatch(2000, 100, TimeUnit.MILLISECONDS);
 
     Point point1 = Point.measurement("cpu")
-      .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
-      .addField("idle", 90L)
-      .addField("user", 9L)
+      //.time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+      .tag("SensorId", "100")
+      .addField("idle", 10L)
+      .addField("user", 11L)
       .addField("system", 1L)
       .build();
-    influxDB.write(dbName, "autogen", point1);
 
-    Query query = new Query("SELECT * FROM cpu", dbName);
+    influxDB.write(DB, "autogen", point1);
+
+
+    BatchPoints batchPoints = BatchPoints.database(DB).build();
+
+    Point point2 = Point.measurement("cpu")
+      //.time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+      .tag("SensorId", "200")
+      .addField("idle", 20L)
+      .addField("user", 22L)
+      .addField("system", 2L)
+      .build();
+
+    Point point3 = Point.measurement("cpu")
+      //.time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+      .tag("SensorId", "300")
+      .addField("idle", 30L)
+      .addField("user", 33L)
+      .addField("system", 3L)
+      .build();
+
+    batchPoints.point(point2);
+    batchPoints.point(point3);
+    influxDB.write(batchPoints);
+
+
+    Query query = new Query("SELECT * FROM cpu", DB);
     QueryResult queryResult = influxDB.query(query);
     System.out.println(queryResult);
 
-    /*
-    Query query = new Query(
-      "SELECT * FROM temperature.celsius.1000000001 WHERE time > '2016-09-18'"
-      , "pobmons");
-    QueryResult queryResult = influxDB.query(query);
-    */
-    //System.out.println(queryResult.getResults().get(0));
+
+    influxDB.close();
   }
 
 
