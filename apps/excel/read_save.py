1c70015ea203cad552e4ba0da28a50ecb7e5a67c
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2017-06-13 04:15:49 +0900

    Create read_save.py

--- apps/excel/read_save.py
+++ apps/excel/read_save.py
@@ -8,11 +8,18 @@
 # http://egloos.zum.com/mcchae/v/11120944
 
 import datetime
+
 import openpyxl
+import xlsxwriter
 import sys
 # sys.path.insert(0, '../doc_design')
 # (to do) have to find how to add different location directory path and file
 # now just using same dir location file
+
+# from openpyxl.workbook import Workbook
+# from openpyxl.writer.excel import ExcelWriter
+# (error) from openpyxl.cell import get_column_letter
+# from openpyxl import load_workbook
 
 class excell_class :
     __ofile = None
@@ -23,7 +30,7 @@ class excell_class :
     #@staticmethod
     def open_exc_doc(self):
         # using unicode file name with u syntax
-        __ofile = openpyxl.load_workbook(u"test.xlsx")
+        __ofile = openpyxl.load_workbook(u"_test__1.xlsx")
         return __ofile
 
     def read_vertical(self, sheet, __start, __end):
@@ -34,49 +41,49 @@ class excell_class :
             for cell in row:
                 v = cell.value
                 if v == None:
-                    continue # do nothing below code, back to next for loop-step
+                    continue # do nothing below code, back to next for loop step
                 __vertical.append(v) # 리스트 __vertical에 아이디 추가
-        return __vertical #
+        return __vertical #__cnt, __cnt_n # 세로 셀 데이터, 데이터 갯수, None 갯수
 
-    # Save to Excel file
-    def save_exc(self, __vdata):
+
+    # 입력 리스트를 액셀에 저장
+    def save_exc (self, __vdata):
         __t = str(datetime.datetime.now())
-
-        workbook = openpyxl.Workbook()
-        worksheet = workbook.create_sheet(title='id_result')
+        workbook = XlsxWriter.Workbook('takeout_id_result'+__t+'.xlsx')
+        worksheet = workbook.add_worksheet()
 
         row = 0
         col = 0
 
         for item in (__vdata):
-            worksheet.cell(column=col, row=row, value=item)
-        workbook.save(filename = 'takeout_id_result'+__t+'.xlsx')
+            worksheet.write(row, col, item)
+            row += 1
+        workbook.close()
 
-    # Save to .py file
-    # You can read data by * import test.py *
-    def save_vdata(__vdata):
-        __t = str(datetime.datetime.now())
-        __odata = 'err_id_list='
-        print str(__vdata)
-        __odata = __odata + str(__vdata)
-        print __odata
 
-        filename = '__err_id_input_list_'+__t+'.py'
-        __ofile = open(filename,"w")
+def save_vdata(__vdata):
+    __t = str(datetime.datetime.now())
+    __odata = 'err_id_list='
+    print str(__vdata)
+    __odata = __odata + str(__vdata)
+    print __odata
 
-        __ofile.write(__odata)
-        __ofile.close()
+    filename = '__err_id_input_list_'+__t+'.py'
+    __ofile = open(filename,"w")
+    __ofile.write(__odata)
+    __ofile.close()
 
 
 if __name__ == "__main__":
+
 # open excell file
     eclass = excell_class()
     op = eclass.open_exc_doc()
     sheets = op.get_sheet_names()
-    sh1 = op.get_sheet_by_name(sheets[0])
-    buf = eclass.read_vertical(sh1,'a4','a101')
 
-    #eclass.save_exc(buf)
-    eclass.save_vdata()
+    sh1 = op.get_sheet_by_name(sheets[0])
+    buf = eclass.read_vertical(sh1,'b1','b541')
+
+    save_vdata(err_buf)
 
     exit (" ...congrats, finish")
