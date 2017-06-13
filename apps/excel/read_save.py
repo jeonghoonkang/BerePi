6c100dacc050310da5b4902c8cd827c411ea2acc
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2017-06-14 02:06:33 +0900

    Create read_save.py

--- apps/excel/read_save.py
+++ apps/excel/read_save.py
@@ -72,6 +72,68 @@ def save_vdata(__vdata):
     __ofile.write(__odata)
     __ofile.close()
 
+    
+    
+    def diff_site_number(__class, target, inv_led):
+    inv_led_sheets = inv_led.get_sheet_names()
+
+    # 인버터 사업장 번호 추출
+    inv_sheet = inv_led.get_sheet_by_name(inv_led_sheets[0])
+    inv_company_nums = __class.read_vertical(inv_sheet, 'b4', 'b101')
+
+    # LED 사업장 번호 추출
+    led_sheet = inv_led.get_sheet_by_name(inv_led_sheets[1])
+    led_company_nums = __class.read_vertical(led_sheet, 'b4', 'b416')
+
+    # 통합파일 사업장 번호 추출
+    target_sheets = target.get_sheet_names()
+    target_sht1 = target.get_sheet_by_name(target_sheets[0])
+    target_company_nums = __class.read_vertical(target_sht1, 'a3', 'a561')
+
+    invs = list()
+    leds = list()
+
+    # 인버터 사업장 번호 비교
+    ibn = len(inv_company_nums)
+    tbn = len(target_company_nums)
+
+    #diff count
+    diff_cnt = 0
+
+    # 유니코드로 변환
+    inv = [unicode(i) for i in inv_company_nums]
+    led = [unicode(i) for i in led_company_nums]
+    target = [unicode(i) for i in target_company_nums]
+
+    # 두개 파일에 존재하는 사업장 번호 검색
+    # KEA 파일에 있는 아이디가 누리텔레콤 파일에 없는 경우 찾음
+    # 리스트의 count 함수 이용. 일치하는 멤버없으면 0 리턴, 존재 갯수 리턴
+    for idx in range(0, tbn):
+        dtect_id = (target[idx])
+        ix_inv = inv.count(dtect_id)
+        ix_led = led.count(dtect_id)
+        # 인버터, LED 리스트에 해당 사업장번호 없음
+        if (ix_inv + ix_led) == 0 :
+            diff_cnt = diff_cnt + 1
+            invs.append([dtect_id, 'is_not_appered_in_NURI_file'])
+
+    # 비교 결과 저장
+    __t = datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S")
+    wb = Workbook()
+    ws = wb.active
+    file_name = '_del_able_result_no_' + __t + '.xlsx'
+    idx = 1
+
+    for item in invs:
+        #if item[2] is None : continue
+        ws["A" + str(idx)] = item[0]
+        ws["B" + str(idx)] = item[1]
+        idx += 1
+
+    wb.save(file_name)
+    wb.close()
+
+    
 
 if __name__ == "__main__":
 
