3d3abc2289d12974098c7f2b4960f75450b93227
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2017-07-12 11:25:22 +0900

    Update read_save.py

--- apps/excel/read_save.py
+++ apps/excel/read_save.py
@@ -7,10 +7,12 @@
 # http://egloos.zum.com/mcchae/v/11120944
 
 import datetime
-
+from openpyxl import Workbook
 import openpyxl
-import xlsxwriter
+import time
+# import XlsxWriter
 import sys
+import importlib
 # sys.path.insert(0, '../doc_design')
 # (to do) have to find how to add different location directory path and file
 # now just using same dir location file
@@ -20,6 +22,9 @@ import sys
 # (error) from openpyxl.cell import get_column_letter
 # from openpyxl import load_workbook
 
+global EE_file
+global EA_file
+
 class excell_class :
     __ofile = None
 
@@ -27,124 +32,106 @@ class excell_class :
         pass
 
     #@staticmethod
-    def open_exc_doc(self):
+    def open_exc_doc(self,__file):
         # using unicode file name with u syntax
-        __ofile = openpyxl.load_workbook(u"_test__1.xlsx")
+        __ofile = openpyxl.load_workbook(__file)
+
+        #pout = "ix: %s, mdsid: %s, donecheck: %d \r" %(ix, mds_id, donecheck)
+        pout = "   ... file opened \n"
+        sys.stdout.write(pout)
+        sys.stdout.flush()
         return __ofile
 
     def read_vertical(self, sheet, __start, __end):
         __vertical = []
-        print " ... Please use column[n]:column[m], vertical read "
+        #print " ... Please use column[n]:column[m], vertical read "
         cell_of_col = sheet[__start:__end]
         for row in cell_of_col:
             for cell in row:
                 v = cell.value
-                if v == None:
-                    continue # do nothing below code, back to next for loop step
+                if v == None: continue # do nothing below code, back to next for loop step
                 __vertical.append(v) # 리스트 __vertical에 아이디 추가
         return __vertical #__cnt, __cnt_n # 세로 셀 데이터, 데이터 갯수, None 갯수
 
-
-    # 입력 리스트를 액셀에 저장
-    def save_exc (self, __vdata):
+    def save_vdata(self,  __list, __lname = None, __ucode = False, _sn = False):
+        #리스트 내부, 숫자를 모두 유니코드로 변환
+        if __ucode == True: __list = [unicode(i) for i in __list]
+        if _sn == True:
+            #사업장번호 포맷 201602260257-01 (15문자, -00 형식으로 맞춤)
+            _n_list=[]
+            for k in __list:
+                if k.find('-') == -1 : k = k + '-01'
+                elif k.find('-0') == -1 : k = k[:-1] + '0' + k[-1:]
+                _n_list.append(k)
+            __list = _n_list
         __t = str(datetime.datetime.now())
-        workbook = XlsxWriter.Workbook('takeout_id_result'+__t+'.xlsx')
-        worksheet = workbook.add_worksheet()
+        __t = ''.join(__t.split())
+        __t = __t[:-15]
+        __odata = 'save_list='
+        if __lname != None : __odata = __lname +'_list='
+        __odata = __odata + str(__list)
+        filename = '__out_'+__t+'.py'
+        if __lname != None : filename = __lname + filename
 
-        row = 0
-        col = 0
+        __ofile = open(filename,"w")
+        print "  file saving ", filename
+        __ofile.write(__odata)
+        __ofile.close()
+        return __list
 
-        for item in (__vdata):
-            worksheet.write(row, col, item)
-            row += 1
-        workbook.close()
+def find_mdsid(__l1, __id, vnum):
 
+    # 엑셀 읽어서, 동일 아이디 찾아서 index 위치 리턴
+    # D:사업장번호 B:미터기번호
+    for k in range(0,len(vnum)):
+        _tmp = vnum[k]
+        if _tmp == None: _tmp = '0'
+        if _tmp[:2] != '20': # 사업장번호 2016으로 시작하도록 확인, 변경
+            vnum[k] = '20'+_tmp
+        #if len(vnum[k]) != 12 : print "  Length is strage ... %d " %len(vnum[k])
 
-def save_vdata(__vdata):
-    __t = str(datetime.datetime.now())
-    __odata = 'err_id_list='
-    print str(__vdata)
-    __odata = __odata + str(__vdata)
-    print __odata
+    found_id = []
+    for k in __l1:
+        print
+        try:
+            print k,
+            f_ix = vnum.index(k) # 리스트에 k 와 동일한 숫자 없으면, except 발생
+            print "found at (%d)" %f_ix
+            found_id.append(__id[f_ix])
+        except:
+            found_id.append(None)
+        time.sleep(0.2)
 
-    filename = '__err_id_input_list_'+__t+'.py'
-    __ofile = open(filename,"w")
-    __ofile.write(__odata)
-    __ofile.close()
+    return found_id
 
-    
-    
-def diff_site_number(__class, target, inv_led):
-    inv_led_sheets = inv_led.get_sheet_names()
-
-    # 인버터 사업장 번호 추출
-    inv_sheet = inv_led.get_sheet_by_name(inv_led_sheets[0])
-    inv_company_nums = __class.read_vertical(inv_sheet, 'b4', 'b101')
-
-    # LED 사업장 번호 추출
-    led_sheet = inv_led.get_sheet_by_name(inv_led_sheets[1])
-    led_company_nums = __class.read_vertical(led_sheet, 'b4', 'b416')
-
-    # 통합파일 사업장 번호 추출
-    target_sheets = target.get_sheet_names()
-    target_sht1 = target.get_sheet_by_name(target_sheets[0])
-    target_company_nums = __class.read_vertical(target_sht1, 'a3', 'a561')
-
-    invs = list()
-    leds = list()
-
-    # 인버터 사업장 번호 비교
-    ibn = len(inv_company_nums)
-    tbn = len(target_company_nums)
-
-    #diff count
-    diff_cnt = 0
-
-    # 유니코드로 변환
-    inv = [unicode(i) for i in inv_company_nums]
-    led = [unicode(i) for i in led_company_nums]
-    target = [unicode(i) for i in target_company_nums]
-
-    # 두개 파일에 존재하는 사업장 번호 검색
-    # EA 파일에 있는 아이디가 EE 파일에 없는 경우 찾음
-    # 리스트의 count 함수 이용. 일치하는 멤버없으면 0 리턴, 존재 갯수 리턴
-    for idx in range(0, tbn):
-        dtect_id = (target[idx])
-        ix_inv = inv.count(dtect_id)
-        ix_led = led.count(dtect_id)
-        # 인버터, LED 리스트에 해당 사업장번호 없음
-        if (ix_inv + ix_led) == 0 :
-            diff_cnt = diff_cnt + 1
-            invs.append([dtect_id, 'is_not_appered_in_NURI_file'])
-
-    # 비교 결과 저장
-    __t = datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S")
-    wb = Workbook()
-    ws = wb.active
-    file_name = '_del_able_result_no_' + __t + '.xlsx'
-    idx = 1
-
-    for item in invs:
-        #if item[2] is None : continue
-        ws["A" + str(idx)] = item[0]
-        ws["B" + str(idx)] = item[1]
-        idx += 1
-
-    wb.save(file_name)
-    wb.close()
-
-    
+def merge_list(__tl, __list1, __list2):
+    if len(__list1) != len(__list2):
+        exit ("[error] Please check the lines of excel file ")
+    __tl = [ [__list1[k], __list2[k]] for k in range(len(__list1)) ]
+    return __tl
 
 if __name__ == "__main__":
 
-# open excell file
-    eclass = excell_class()
-    op = eclass.open_exc_doc()
-    sheets = op.get_sheet_names()
+    print "  Working... please input file name, like *anyname.py* "
+    volubility = len(sys.argv)
+    _t_string_ = " ".join(sys.argv)
 
-    sh1 = op.get_sheet_by_name(sheets[0])
-    buf = eclass.read_vertical(sh1,'b1','b541')
+    if volubility > 1:
+        _t_string = sys.argv[1]
+        print "  input file is", _t_string
+    else : exit (" We need excel file which contains MDS_ID")
 
-    save_vdata(err_buf)
+    #exit("  It is OK, debug exit")
+    # 입력파일은 실행할때, arg 로 입력함
+    E1_file = _t_string
 
-    exit (" ...congrats, finish")
+    exl_class = excell_class()
+    e1 = exl_class.open_exc_doc(E1_file)
+    sheets = e1.get_sheet_names()
+    sh1 = e1.get_sheet_by_name(sheets[0])
+    lst1 = exl_class.read_vertical(sh1,'a1','a500')
+    u1 = exl_class.save_vdata(lst1,'_id_list_',True)
+
+    idlist = u1
+
+    print " Loss ", float(100* idlist.count(None) / len(idlist)), '%', idlist.count(None), len(idlist)
