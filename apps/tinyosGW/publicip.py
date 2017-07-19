7df92e0675ac92836f56285b9f8f89a82e2ebd6d
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2017-07-19 22:29:20 +0900

    public ip update

--- apps/tinyosGW/publicip.py
+++ apps/tinyosGW/publicip.py
@@ -5,6 +5,7 @@
 from __future__ import print_function
 from subprocess import *
 from types import *
+import sys
 
 def run_cmd(cmd):
     p = Popen(cmd, shell=True, stdout=PIPE)
@@ -17,18 +18,27 @@ def getip():
     return ip
 
 def getiip():
-    cmd = "ifconfig"
-    ip = run_cmd(cmd)
-    return ip
+    cmd = "/sbin/ifconfig"
+    iip = run_cmd(cmd)
+    return iip
+
+def writeFile(_in, fn = 'ip.txt'):
+    f = open(fn, 'w')
+    f.write(_in)
+    f.flush()
+    f.close()
+    return
 
 if __name__ == '__main__':
     p_ip = getip()
     i_ip = getiip()
-    info = p_ip + i_ip
-    fname = '/home/USER/my_deamon/output/ip.html'
-    writeFile (info, fname )
-    cmd = "scp" + " %s" %fname + " pi@AAA.iptime.org:" + "/var/www/html/server/" + fname[-7:]
+    info = i_ip + p_ip 
+    fname = '/home/tinyos/my_daemon/output/2T_.txt'
 
-    cmd = "scp" + " ip.html" + " pi@iptime.org:" + "/var/www/html/ip.html" 
-    print (cmd)
-    print (run_cmd(cmd))
+    writeFile (info, fname)
+    
+    cmd = "scp" + " %s" %fname + " pi@.iptime.org:" + "/var/www/html/server/" 
+    ret = run_cmd(cmd)
+    print (" ")
+    print (ret)
+
