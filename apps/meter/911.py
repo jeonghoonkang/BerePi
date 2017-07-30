372baa64cabea08f75782e79a018e42ae3d0d33d
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2017-07-30 16:36:53 +0900

    Update 911.py

--- apps/meter/911.py
+++ apps/meter/911.py
@@ -7,14 +7,16 @@ from matplotlib.dates import HourLocator, MinuteLocator, DateFormatter, date2num
 import datetime
 import pylab
 import time, sys, os
+#from datetime import timedelta
 
-devel_dir = "/home/tinyos/devel"
-tmp_dir = devel_dir+"/keti_tinyos/danalytics/thingsweb/weblib/recv"
+devel_dir = "/home/pi/devel"
+tmp_dir = devel_dir+"/danalytics/thingsweb/weblib/recv"
 sys.path.append(tmp_dir)
 
 from lastvalue import *
 
 day_delta = 0
+tmp_delta = 0
 
 def currentMonthValue(nodeid) :
     ## collect DB data from MySQL DB to row
@@ -97,188 +99,10 @@ def lastMonthValue(nodeid) :
       m2ago = nowDate.month - 1
       m1ago = nowDate.month 
 
+    #datetime.datetime
     findingDateM1ago = nowDate.replace(month=m1ago,day=payCountDay,hour=0,minute=0,second=0)
     findingDateM2ago = nowDate.replace(month=m2ago,day=payCountDay,hour=0,minute=0,second=0)
-    # just before time
+
+    # convert to datetime.time
     queryDayM1ago = datetime.date(findingDateM1ago.year, findingDateM1ago.month, findingDateM1ago.day)
-    # older time
     queryDayM2ago = datetime.date(findingDateM2ago.year, findingDateM2ago.month, findingDateM2ago.day)
-
-    queryToday = datetime.date(nowDate.year, nowDate.month, nowDate.day)
-    
-    print
-    _2month_before = str(queryDayM2ago)
-    _2month_before = _2month_before.replace('-','/') + '-00:00:'
-
-    _1month_before = str(queryDayM1ago)
-    _1month_before = _1month_before.replace('-','/') + '-00:00:'
-
-    print 
-    print 'from ' + _1month_before[:10],
-    print ' to now ' + str(nowDate)[:16], 
-    print ', considering ' + _2month_before[:10]
-    print
-    
-    dbip="...53:4242"
-   
-    _2month_before_30 = _2month_before
-
-
-    tmp_val0 = get_value(dbip, 'gyu_RC1_etype.t_current', {'nodeid':'911'}, _2month_before+'00',_2month_before+'30')
-    tmp_val1 = get_value(dbip, 'gyu_RC1_etype.t_current', {'nodeid':'911'}, _1month_before+'00',_1month_before+'30')
-    tmp_val2 = get_last_value(dbip, 'gyu_RC1_etype.t_current', {'nodeid':'911'})
-
-    current_wattH = int(tmp_val2[0])-int(tmp_val1[0])
-    lastmonth_wattH = int(tmp_val1[0])-int(tmp_val0[0])
-
-    global day_delta
-    day_delta = nowDate.day - payCountDay
-
-    # calc, how many days are passed from previous pay_count_day
-    # to do : add code for 26, same day, not 1 day passed yet
-    if (day_delta > 0) :
-        passed_day = day_delta
-    else :
-        passed_day = 30 + day_delta 
-
-    #print "delta", day_delta
-    #print "passed day", passed_day
-    #print "current wattH", current_wattH
-
-    est_watt = (current_wattH/1000.0 * (30.0 / passed_day)) 
-
-    print "current watt : %d Wh" %(current_wattH/1000.0)
-    calcPay(current_wattH/1000.0)
-    
-    print "expected watt on next day of 26, is %d Wh" %est_watt
-    #print "Money %d Won" %calcPay(est_watt)
-    calcPay(est_watt)
-    
-    print
-    print "previous Month watt : %d Wh" %(lastmonth_wattH/1000.0)
-    calcPay(lastmonth_wattH/1000.0)
-
-    print
-    print "30,000 won, 233 Wh"
-    calcPay(233.0)
-
-    exit ("under develplemnt now")
-
-    ## Accumulated Watt Hour
-
-    ## excute Query 
-    ## if Watth is lack of data, it means DB server does not have data
-    ### should check DB schema in order to use exact order of WattH[x]
-    preVal = current_tot_etype[0]
-    print ' 2 Month ago Meter Valud : %d ' % preVal
-
-    #preMonMeter = ((curVal-preVal)/1000.0)
-    #print ' Previous Month Usage : %f kWh ' % preMonMeter 
-
-    return preMonMeter
-
-
-def calcPay(meterVal):
-    # print "[msg]......calling funciton... calcPay()",
-    # print calcPay 
-	# test value 
-    # meterVal = 630
-    payTable_level=['1L', '2L', '3L', '4L', '5L', '6L']
-    payTable_base=[410, 910, 1600, 3850, 7300, 12940]
-    payTable_multiplier=[60.7, 125.9, 187.9, 280.6, 417.7, 709.5]
-
-    if (meterVal > 500.0):
-      payIndex = 5
-    elif (meterVal > 400.0):
-      payIndex = 4
-    elif (meterVal > 300.0):
-      payIndex = 3
-    elif (meterVal > 200.0):
-      payIndex = 2
-    elif (meterVal > 100.0):
-      payIndex = 1
-    else :
-      payIndex = 0
-
-    #print 'debug PAYINDEX = %d ' % payIndex
-    korwon = 0
-    onCalc = 0
-    lastCalc = 0
-    last_loop = 0
-
-    last_loop = int(meterVal // 100) + 1
-    #print last_loop
-
-    # 1 step : calc meterVal 0 ~ 100
-    if meterVal < 100.0 : 
-        lastCalc = payTable_base[0] + ( payTable_multiplier[0] * (meterVal) )
-
-    # 2 step : calc meterVal 100 ~ 599, 2 < last_loop < 7
-    elif (last_loop < 7) : 
-        for loop in range(0, payIndex) :
-            onCalc += (payTable_multiplier[loop] * 100)
-        lastCalc = onCalc + payTable_base[payIndex] + ( payTable_multiplier[payIndex] * (meterVal%100) )
-
-    # 3 step : calc meterVal beyond 600, last_loop > 6
-	# test value : 630 -> 241560, last_loop == 7
-    elif (last_loop > 6) :
-         for loop in range(0, 6) : # 0 ~ 5 payIndex 
-            onCalc += (payTable_multiplier[loop] * 100)
-         for loop in range(6, last_loop-1) : # 6 ~ something payIndex
-            onCalc += (payTable_multiplier[5] * 100)
-         lastCalc = onCalc + payTable_base[5] + ( payTable_multiplier[5] * (meterVal%100) )
-
-    #print onCalc
-    #print lastCalc
-
-    korwon = lastCalc + (lastCalc*0.137)
-    print ' Pay KOR_WON = %d ' % korwon
-    print
-    return korwon
-
-
-def closingTask():
-    cursor.close()
-    db.close()
-    print " Closing DB sessions. all done successfully "
-    print
-
-#if __name__== "__main__" :
-
-result = [[], [], [], [], [], [], []]
-dates = [[], [], [], [], [], [], []]
-x = [[], [], [], [], [], [], []]
-row, datetime_ = [], []
-# delete due to MySQL
-#db = MySQLdb.connect("...51", "keti", "keti123!@#", "PEAKSAVE")
-#cursor = db.cursor()
-
-EtypeId = 911
-payCountDay = 26
-
-ret = "Meter ID = " + str(EtypeId)+",  "
-ret = ret + "Pay check day is " + str(payCountDay) + " " + "<br>"
-
-nodeid = EtypeId 
-
-# important functions are here
-# (func) lastMonthValue(nodeid)
-# (func) calcPay(meterVal)
-# (func) currentMonthValue(nodeid)
-
-# current_etype = get_last_value(dbip,'gyu_RC1_etype.current',{'nodeid':'911'})
-# current_tot_etype = get_last_value(dbip,'gyu_RC1_etype.t_current',{'nodeid':'911'})
-
-#print "<Content-Type: text/html;charset=utf-8>"
-#print '<font size="20">'
-
-meterVal = lastMonthValue(nodeid)
-ret = ret + " Previous month payment = " + str (calcPay(meterVal)) + " KOR WON" + "<br><br> "
-
-meterVal = currentMonthValue(nodeid)
-ret = ret + " This Month = <br> <font size=12> " + str (meterVal) + " kW" + "<br><br> "
-ret = ret + str (calcPay(meterVal)) + " KOR WON <br>" + " </font><br> is this month payment until now<br> "  + "<br> "
-ret = ret + str (datetime.datetime.today()) + "<br> "
-
-#closingTask()
-
