df2aad75355ccb4dad1837f948739914b0555ee8
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2017-09-03 19:58:37 +0900

    Update rsync.sh

--- setup/shell/rsync.sh
+++ setup/shell/rsync.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
+#입력인자 $1, -z 길이가 0인지 확인
 if [ -z $1 ] ;
 then
     rsync -avhz --progress * tinyos@10.0.1.189:/var/www/webdav/data/send
 else
     rsync -avhz --progress $1 tinyos@10.0.1.189:/var/www/webdav/data/send
+fi
