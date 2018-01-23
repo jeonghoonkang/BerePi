1910bfe882f2889539c3d521a0ee69ee1f737f73
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-01-23 22:47:58 +0900

    Create install_java_ubuntu.sh

---
+++ apps/otsdb/install_java_ubuntu.sh
@@ -0,0 +1,13 @@
+sudo apt-get purge openjdk*
+
+sudo add-apt-repository ppa:webupd8team/java
+
+sudo apt-get update
+
+#Java 8 설치
+sudo apt-get install oracle-java8-installer
+#Java 7 설치
+#sudo apt-get install oracle-java7-installer
+#Java 6 설치
+#sudo apt-get install oracle-java6-installer
+
