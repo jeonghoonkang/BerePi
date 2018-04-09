ab75d4f7444788ff419ecf56461e60f9bf911ae6
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-04-09 18:29:43 +0900

    Update put_test.py

--- apps/otsdb/put/put_test.py
+++ apps/otsdb/put/put_test.py
@@ -12,13 +12,10 @@ import requests
 import json
 import subprocess
 
-
-################################
-# example : url = "http://10.0.0.43:4242/api/put"
-# warning : we have to 50 JSON pack to put in OpenTSDB, on first stage.
-#           if you add more, you shoud test amount of TX packets
-################################
-
+'''  example : url = "http://10.0.0.43:4242/api/put" '''
+     warning : we have to 50 JSON pack to put in OpenTSDB, on first stage. 
+           if you add more, you shoud test amount of TX packets '''
+           
 def otsdb_restful_put(url):
     sname = "kang-tinyos-test-000"
     toend = 101
