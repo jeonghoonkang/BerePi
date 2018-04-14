d75a9e28986da6e1d92976f1472ddb8b58ae498e
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-04-14 16:26:19 +0900

    Update key.sh

--- setup/ssh/key.sh
+++ setup/ssh/key.sh
@@ -1,3 +1,11 @@
 
 cat ~/.ssh/id_rsa.pub | ssh -p 9999 tinyos@99.99.com 'cat>>/home/tinyos/.ssh/authorized_keys'
 
+#!/bin/bash
+
+export ain=$1
+echo $ain
+
+if [ -z $ain ];
+then
+    find ./ -name "*.flac" -print0 | xargs -0 -i -t ffmpeg -i {} -codec:a libmp3lame -b:a 256k {}.mp3
