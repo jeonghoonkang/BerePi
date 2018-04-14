22e48e3efe612c354fe3e8020e33bd536c5dd3c5
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-04-14 17:27:32 +0900

    ssh shell

--- setup/ssh/key.sh
+++ setup/ssh/key.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+echo "usage : sh key.sh {IP_ADD} {PORT}, {} : user should input"
+export ip=$1
+export port=$2
 
-export ain=$1
-echo $ain
+echo $ip, $port
 
-if [ -z $ain ];
+if [ -z $ip ];
 then #아규먼트가 없으면 실행
     echo "you need to input IP and port number"
-#    find ./ -name "*.flac" -print0 | xargs -0 -i -t ffmpeg -i {} -codec:a libmp3lame -b:a 256k {}.mp3
-
+#    find ./ -name "*.flac" -print0 | xargs -0 -i -t ffmpeg -i {}
+#            -codec:a libmp3lame -b:a 256k {}.mp3
 else #아규먼트가 있으면
-    cat ~/.ssh/id_rsa.pub | ssh -p 9999 tinyos@99.99.com 'cat>>/home/tinyos/.ssh/authorized_keys'
+    ssh-keygen
+    cat ~/.ssh/id_rsa.pub | ssh -p $port tinyos@$ip 'cat>>/home/tinyos/.ssh/authorized_keys'
 fi
