562717d64ac2fe1a32fe37fe9ff7a931341a1e64
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-04-15 01:09:52 +0900

    ssh shell python

--- setup/ssh/sshkey.py
+++ setup/ssh/sshkey.py
@@ -3,10 +3,10 @@
 
 from __future__ import print_function
 import subprocess
+import os
 import sys
 
 if __name__ == '__main__':
-
 
     print ("usage : python sshkey.py {IP_ADD} {PORT}, {} : user should input")
 
@@ -20,18 +20,17 @@ if __name__ == '__main__':
     ip = arg1
     port = arg2
     id = arg3
-    print ("...running", " inputs are ", ip, port, id)
+    print ("... start running", " inputs are ", ip, port, id)
 
+    print ("... key generating")
+    os.system('ssh-keygen')
 
+    run_cmd = "cat '/home/tinyos/.ssh/id_rsa.pub'"
+    run_cmd += " | ssh -p %s %s@%s" %(port, id, ip)
+    run_cmd += " 'cat>>/home/%s/.ssh/authorized_keys'" %id
+    print (run_cmd)
+    os.system(run_cmd)
 
-'''
-if [ -z $ip ];
-then #아규먼트가 없으면 실행
-    echo "you need to input IP and port number"
-#    find ./ -name "*.flac" -print0 | xargs -0 -i -t ffmpeg -i {}
-#            -codec:a libmp3lame -b:a 256k {}.mp3
-else #아규먼트가 있으면
-    ssh-keygen
-    cat ~/.ssh/id_rsa.pub | ssh -p $port tinyos@$ip 'cat>>/home/tinyos/.ssh/authorized_keys'
-fi
-'''
+    ''' 아래 코드는 동작을 안함. 확인 필요 '''
+    #ret = subprocess.check_output(run_cmd)
+    #print (ret)
