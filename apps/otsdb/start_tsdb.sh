49a52e2ba0b2ca0e7bcdff69463d71360335d1ed
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-05-08 08:06:32 +0900

    Update start_tsdb.sh

--- apps/otsdb/start_tsdb.sh
+++ apps/otsdb/start_tsdb.sh
@@ -11,15 +11,14 @@ sudo sh ./bin/start-hbase.sh && sleep 90 # raspberryPi zero 에서는 90초 정�
 cd /usr/local/opentsdb
 sudo screen -dmS tsd_start sudo sh /usr/local/opentsdb/build/tsdb tsd --port=4242 --staticroot=/usr/local/opentsdb/build/staticroot --cachedir=/usr/local/opentsdb/tmp --auto-metric
 
-sleep 10
+# 변경사항. 2018-05-8
+#sleep 10
 
-cd /usr/local/tcollector
-sudo ./startstop start
+#cd /usr/local/tcollector
+#sudo ./startstop start
 
-echo "[BerePi] last step of run openTSDB, tcollector by BerePi start-up script"
-echo "------------------------------------------------------------------------"
-echo " "
-
-# 변경사항. 2017-05-29
+#echo "[BerePi] last step of run openTSDB, tcollector by BerePi start-up script"
+#echo "------------------------------------------------------------------------"
+#echo " "
 #     cd tcollector
 #     sudo python tcollector.py -H <TSDB Host IP> -p <TSDB port> -D
