8b22edf1be63d57135c686cabd5371c6b390fab0
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-05-16 22:25:26 -0700

    apt setup update

--- setup/setup_apt.sh
+++ setup/setup_apt.sh
@@ -3,52 +3,52 @@
 
 #sudo apt-get -y install subversion
 
-# sudo apt-get -y install htop
-# sudo apt-get -y install screen
-# sudo apt-get -y install rsync
+ sudo apt-get -y install htop
+ sudo apt-get -y install screen
+ sudo apt-get -y install rsync
 # sudo apt-get -y install vim
-# sudo apt-get -y install tree
-# sudo apt-get -y install nmap
-# sudo apt-get -y install unzip
-# sudo apt-get -y install dnsutils
-# sudo apt-get -y install curl
-# sudo apt-get -y install dcfldd pv
-# sudo apt-get -y install lrzsz
-# sudo apt-get -y install lynx
+ sudo apt-get -y install tree
+ sudo apt-get -y install nmap
+ sudo apt-get -y install unzip
+ sudo apt-get -y install dnsutils
+ sudo apt-get -y install curl
+ sudo apt-get -y install dcfldd pv
+ sudo apt-get -y install lrzsz
+ sudo apt-get -y install lynx
 # sudo apt-get -y install ntpdate
 # sudo apt-get -y install vim-gnome
-# sudo apt-get -y install dos2unix
+ sudo apt-get -y install dos2unix
 # sudo apt-get -y install dosbox
-# sudo apt-get -y install wicd wicd-curses
+ sudo apt-get -y install wicd wicd-curses
 # sudo apt-get -y install samba samba-common-bin
 # sudo apt-get -y install php5 libapache2-mod-php5
 # sudo apt-get -y install mysql-server php5-mysql
-# sudo apt-get -y install postfix
-# sudo apt-get -y install rrdtool
+ sudo apt-get -y install postfix
+ sudo apt-get -y install rrdtool
 
- wget https://pypi.python.org/packages/source/d/distribute/distribute-0.7.3.zip
- unzip distribute-0.7.3.zip
- cd distribute-0.7.3
- python setup.py install
- easy_install pip
+wget https://pypi.python.org/packages/source/d/distribute/distribute-0.7.3.zip
+unzip distribute-0.7.3.zip
+cd distribute-0.7.3
+sudo  python setup.py install
+sudo easy_install pip
 
-# sudo python setup.py install
-# sudo easy_install pip
-
-# sudo apt-get -y install python-tk
+sudo apt-get -y install python-tk
+sudo apt-get -y install python-dev
 # sudo apt-get -y install python-matplotlib
 # sudo apt-get -y install python-mysqldb
 # sudo apt-get -y install python-smbus
 # sudo apt-get -y install python-requests
-# sudo apt-get -y install python-dev
 # sudo apt-get -y install python-twisted
-# sudo pip install requests --upgrade
-# sudo pip install twisted --upgrade
-# sudo pip install numpy
-# sudo pip install networkx
-# sudo pip install httplib
-# sudo pip install urllib3
-# sudo pip install utils
+sudo pip install requests --upgrade
+sudo pip install twisted --upgrade
+sudo pip install numpy
+sudo pip install networkx
+sudo pip install httplib
+sudo pip install urllib3
+sudo pip install utils
+sudo pip install scipy
+sudo pip install scikit-learn
+
 # (for PIP)
 # python -m ensurepip --default-pip
 
