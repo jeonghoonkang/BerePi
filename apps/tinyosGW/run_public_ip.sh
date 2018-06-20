35294b9a91d741e1d931937f9d90ee2e7a17dbee
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-06-20 14:04:15 +0900

    Update run_public_ip.sh

--- apps/tinyosGW/run_public_ip.sh
+++ apps/tinyosGW/run_public_ip.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 # Author : jeonghoonkang, https://github.com/jeonghoonkang
 export cmd='python /home/pi/devel/BerePi/apps/tinyosGW/publicip.py {ip or URL} {port} {ID} {password}'
-echo cmd
-exec cmd
+echo $cmd
+exec $cmd
