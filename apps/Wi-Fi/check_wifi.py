90f2eb45828139e460b6fa9e61f82c1c90c5f74f
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-06-23 15:35:52 +0900

    Add files via upload

---
+++ apps/Wi-Fi/check_wifi.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import time
+import datetime
+
+while(1):
+    print os.system('sudo sh wifi_cron.sh')
+    #print os.system('grep CRON /var/log/syslog')
+    print datetime.datetime.now()
+    time.sleep(300)
+
