919fd13eaa1e04b288ff8a709326684a9de8c583
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-06-26 00:28:13 +0900

    ing... png file download func

---
+++ apps/otsdb/read/get_png.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Author : https://github.com/jeonghoonkang
+
+import urllib
+#http://URL:PORT/api/query?start=2018/06/25-00:00:00&end=2018/06/26-00:00:00&m=none:keti.tinyos.packet.test?png
+
+# main function
+if __name__ == "__main__":
+    print "...starting..."
+    ip_port =  'www.abc.com:4242'
+    url = 'http://' + ip_port + '/q?start=2018/06/25-13:55:00&end=2018/06/25-14:16:34&m=none:keti.tinyos.packet.test&o=&wxh=800x600&style=linespoint&png'
+    print url
+    urllib.urlretrieve(url, unicode('download.png', "949") )
+    print "\n ...ending..."
