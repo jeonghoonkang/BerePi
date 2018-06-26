b173d8ff0cdbb05dfbdc53c04561c588082d15e0
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-06-27 01:13:20 +0900

    Update get_png.py

--- apps/otsdb/read/get_png.py
+++ apps/otsdb/read/get_png.py
@@ -4,7 +4,6 @@
 import urllib
 #http://URL:PORT/api/query?start=2018/06/25-00:00:00&end=2018/06/26-00:00:00&m=none:keti.tinyos.packet.test?png
 
-# main function
 if __name__ == "__main__":
     print "...starting..."
 
