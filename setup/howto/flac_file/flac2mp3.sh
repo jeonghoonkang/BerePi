02ea8675790912a315f02770aff6d6025957a881
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-07-08 18:43:57 +0900

    Update flac2mp3.sh

--- setup/howto/flac_file/flac2mp3.sh
+++ setup/howto/flac_file/flac2mp3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export ain=$1
-echo $ain
-
+echo $ainc
+eho "starting fiac file"
 if [ -z $ain ];
 then
     find ./ -name "*.flac" -print0 | xargs -0 -i -t ffmpeg -i {} -codec:a libmp3lame -b:a 256k {}.mp3
