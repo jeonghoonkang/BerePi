bddd8957d05f1d718a83f6ff7446aad7075262b7
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-07-10 15:28:54 +0900

    add script for server info put to management server

--- apps/tinyosGW/run_public_ip.sh
+++ apps/tinyosGW/run_public_ip.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/bash
 # Author : jeonghoonkang, https://github.com/jeonghoonkang
-export cmd='python /home/pi/devel/BerePi/apps/tinyosGW/publicip.py {ip or URL} {port} {ID} {password}'
-echo $cmd
-exec $cmd
+export ip=''
+export port=''
+export id=''
+export passwd=''
+export ip=$1
+export port=$2
+export id=$3
+export passwd=$4
+
+echo $ip
+echo $port
+echo $id
+echo $passwd
+
+#export cmd='python /home/pi/devel/BerePi/apps/tinyosGW/publicip.py {ip or URL} {port} {ID} {password}'
+export cmd='python /home/tinyos/devel/BerePi/apps/tinyosGW/publicip.py '$ip' '$port' '$id' '$passwd
+echo $cmd 
+exec $cmd 
