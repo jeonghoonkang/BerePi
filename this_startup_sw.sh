0f0d8c66c79931652fe6f7264a000ee173c38f3d
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-07-19 13:33:35 +0900

    startup script

---
+++ this_startup_sw.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Author : https://github.com/jeonghoonkang
+
+alias sr='screen'
+
+sudo screen -dmS startup sudo bash /home/pi/devel/BerePi/apps/otsdb/start_tsdb.sh
+#sudo screen -dmS lcd sudo python /home/pi/devel/BerePi/apps/lcd_berepi/watch.py -ip xxxxx
+
