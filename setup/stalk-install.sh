8dad95776fe745a484bf63ffe121232a26523073
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-07-26 16:24:51 +0900

    stalk install

--- setup/stalk-install.sh
+++ setup/stalk-install.sh
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 # Author : Jeonghoonkang, github.com/jeonghoonkang
 
-wget https://raw.githubusercontent.com/321core/STALK-INSTALL/master/install-ubuntu.sh
-sh install-ubuntu.sh
+wget https://raw.githubusercontent.com/321core/STALK-INSTALL/master/install-raspberrypi.sh
+sh install-raspberrypi.sh
