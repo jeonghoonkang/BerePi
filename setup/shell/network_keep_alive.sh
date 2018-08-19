b0901a36336006edfd3bef956826ed57ca7b8e6e
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-08-20 08:19:03 +0900

    Update network_keep_alive.sh

--- setup/shell/network_keep_alive.sh
+++ setup/shell/network_keep_alive.sh
@@ -1,2 +1,4 @@
 #라즈베리파이3 부터는 해당 코드가 있어야 지속적으로 네트워크 유지함. 부팅시에 원인 파악이 안된 에러때문에, 네트워크 연결이 안되는 경우가 있음 
+#아래 코드를 /etc/rc.local 에 넣어서 재부팅시에 네트워크 접속이 되도록 실행하여 문제 
 sudo systemctl restart dhcpcd
+sudo systemctl daemon-reload
