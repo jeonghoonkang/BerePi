a0c0adbf86e82447f879b331ec79392edca12e52
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-08-22 15:52:41 +0900

    Update this_startup_sw.sh

--- this_startup_sw.sh
+++ this_startup_sw.sh
@@ -5,4 +5,4 @@
 alias sr='screen'
 #sudo screen -dmS startup sudo bash /home/pi/devel/BerePi/apps/otsdb/start_tsdb.sh
 #sudo screen -dmS lcd sudo python /home/pi/devel/BerePi/apps/lcd_berepi/watch.py -ip xxxxx
-sudo /home/pi/devel/BerePi/apps/tinyosGW/run_public_ip.sh tinyos.iptime.org 22 pi tinyos0
+#sudo /home/pi/devel/BerePi/apps/tinyosGW/run_public_ip_rpi.sh xxx xxx xxx 
