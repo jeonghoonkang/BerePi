1fd2497a0814a7b79485e80ccabf7b24e5197be0
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-08-23 11:37:39 +0900

    Update startup_sw.sh

--- startup_sw.sh
+++ startup_sw.sh
@@ -6,18 +6,16 @@ alias sr='screen'
 
 #sudo screen -dmS startup sudo sh /home/pi/devel/BerePi/apps/otsdb/start_tsdb.sh
 #sudo screen -dmS lcd sudo python /home/pi/devel/BerePi/apps/lcd_berepi/watch.py -ip xxxxx
+#/home/pi/devel/BerePi/apps/tinyosGW/run_pubic_ip.sh {ip/url} {port} {login id} {pw}
 
 
-####
+
+#### Please add below line on /etc/rc.local
+# sudo sh /home/pi/devel/BerePi/this_startup_sw.sh
+#### in other word,
 # in /etc/rc.local
 ####
 # startup software by BerePi
 #sudo sh /home/pi/devel/BerePi/startup_sw.sh
 #exit0
 ####
-
-#### Please add below line on /etc/rc.local
-# sudo sh /home/pi/devel/BerePi/this_startup_sw.sh
-
-
-#/home/pi/devel/BerePi/apps/tinyosGW/run_pubic_ip.sh {ip/url} {port} {login id} {pw}
