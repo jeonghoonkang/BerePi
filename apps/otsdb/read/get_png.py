34237dbbcb4c01bb722f74e66130c64ee7250463
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-09-06 10:59:59 +0900

    Update get_png.py

--- apps/otsdb/read/get_png.py
+++ apps/otsdb/read/get_png.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Author : https://github.com/jeonghoonkang
+# please use the script : run_get_png.sh
 
 import urllib
 #http://URL:PORT/api/query?start=2018/06/25-00:00:00&end=2018/06/26-00:00:00&m=none:keti.tinyos.packet.test?png
