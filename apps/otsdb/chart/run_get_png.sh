b4868965145ea7818d7f18663d818b28ee0cedd1
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-09-08 01:06:07 +0900

    Update run_get_png.sh

--- apps/otsdb/chart/run_get_png.sh
+++ apps/otsdb/chart/run_get_png.sh
@@ -1,2 +1,2 @@
-#http://URL:PORT/api/query?start=2018/06/25-00:00:00&end=2018/06/26-00:00:00&m=none:keti.tinyos.packet.test?png
+#http://URL:PORT/q?start=2018/06/25-00:00:00&end=2018/06/26-00:00:00&m=none:keti.tinyos.packet.test?png
 #python get_png.py 
