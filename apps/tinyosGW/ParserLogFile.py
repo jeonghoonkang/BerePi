b6138cdac4e0dee578acf1b4969622571cc3766a
ipmstyle <ipmstyle@gmail.com>
2018-09-10 23:43:17 +0900

    update ParserLogFile.py

--- apps/tinyosGW/ParserLogFile.py
+++ apps/tinyosGW/ParserLogFile.py
@@ -2,34 +2,24 @@
 
 import re
 
-LOGFILE = "*.log"
+#LOGFILE = "*.log"
 LOGFILE = "/home/pi/rpi_log.log"
-PATTERN = re.compile("[INFO] (\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2}),(\d{3}) > ")
-PATTERN = re.compile("[INFO] \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3} >")
-PATTERN = re.compile('[INFO]..\d{4}-\d{2}-\d{2}')
 PATTERN = re.compile("\[INFO\] (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}) > (.+)\n", re.DOTALL)
+RPIINFO = ['HOSTNAME', 'IP', 'HDD', 'FREE MEM', 'CPU(\(.+\))', 'MEM(\(.+\))']
 
 CNT = 0
 
 f = open(LOGFILE,"r")
 
-
 lines = f.readlines()
 for line in lines:
     if PATTERN.search(line):
         CNT+=1
-        print re.split(PATTERN, line)
+        rpi_info = re.split(PATTERN, line)
+    else:
+        #rpi_info.append(line)
+        rpi_info = [info for info in rpi_info if len(info) > 0]
+        print rpi_info
 print CNT
 
-
-#data = f.read()
-#print re.search(PATTERN, data)
-#packet = re.split(PATTERN, data)
-
-#for p in packet:
-#    pass
-    #print p
-#print len(packet)
-#print re.search(PATTERN, data)
-#print packet.group(1)
 f.close()
