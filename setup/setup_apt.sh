0bba7834b430465523e3896adc4075e9e72dc1a0
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-09-13 20:28:34 +0900

    update setup apt shell script

--- setup/setup_apt.sh
+++ setup/setup_apt.sh
@@ -3,41 +3,38 @@
 
 #sudo apt-get -y install subversion
 
-# sudo apt-get -y install htop
-# sudo apt-get -y install screen
-# sudo apt-get -y install rsync
-# sudo apt-get -y install vim
-# sudo apt-get -y install tree
-# sudo apt-get -y install nmap
-# sudo apt-get -y install unzip
-# sudo apt-get -y install dnsutils
-# sudo apt-get -y install curl
-# sudo apt-get -y install dcfldd pv
-# sudo apt-get -y install lrzsz
-# sudo apt-get -y install lynx
-# sudo apt-get -y install ntpdate
-# sudo apt-get -y install vim-gnome
-# sudo apt-get -y install dos2unix
+sudo apt-get -y install htop
+sudo apt-get -y install screen
+sudo apt-get -y install rsync
+sudo apt-get -y install vim
+sudo apt-get -y install tree
+sudo apt-get -y install nmap
+sudo apt-get -y install unzip
+sudo apt-get -y install dnsutils
+sudo apt-get -y install curl
+sudo apt-get -y install dcfldd pv
+sudo apt-get -y install lrzsz
+sudo apt-get -y install lynx
+sudo apt-get -y install ntpdate
+sudo apt-get -y install vim-gnome
+sudo apt-get -y install dos2unix
 # sudo apt-get -y install dosbox
-# sudo apt-get -y install wicd wicd-curses
-# sudo apt-get -y install samba samba-common-bin
-# sudo apt-get -y install php5 libapache2-mod-php5
-# sudo apt-get -y install mysql-server php5-mysql
-# sudo apt-get -y install postfix
-# sudo apt-get -y install rrdtool
+sudo apt-get -y install wicd wicd-curses
+sudo apt-get -y install samba samba-common-bin
+sudo apt-get -y install php5 libapache2-mod-php5
+sudo apt-get -y install mysql-server php5-mysql
+sudo apt-get -y install postfix
+sudo apt-get -y install rrdtool
 
-# sudo apt-get -y install fcitx-hangul
-# sudo apt-get -y install fonts-unfonts-core
+sudo apt-get install fcitx-hangul
 
+sudo apt-get -y install fonts-unfonts-core
 
-# wget https://pypi.python.org/packages/source/d/distribute/distribute-0.7.3.zip
-# unzip distribute-0.7.3.zip
-# cd distribute-0.7.3
-# sudo  python setup.py install
-# sudo easy_install pip
-
-# pip install markerlib
-# pip install -U setuptools
+wget https://pypi.python.org/packages/source/d/distribute/distribute-0.7.3.zip
+unzip distribute-0.7.3.zip
+cd distribute-0.7.3
+sudo  python setup.py install
+sudo easy_install pip
 
 sudo apt-get -y install python-tk
 sudo apt-get -y install python-dev
@@ -54,10 +51,11 @@ sudo pip install networkx
 sudo pip install httplib
 sudo pip install urllib3
 sudo pip install utils
-sudo pip install scipy
 sudo pip install scikit-learn
 sudo pip install pandas
 
+sudo apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran
+sudo pip install scipy
 # (for PIP)
 # python -m ensurepip --default-pip
 
