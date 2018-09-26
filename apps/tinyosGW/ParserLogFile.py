9cb981dda7be223b78b63e260e499ae8de81196a
ipmstyle <ipmstyle@gmail.com>
2018-09-26 22:38:13 +0900

    update ParserLogFile.py

--- apps/tinyosGW/ParserLogFile.py
+++ apps/tinyosGW/ParserLogFile.py
@@ -10,16 +10,21 @@ RPIINFO = ['HOSTNAME', 'IP', 'HDD', 'FREE MEM', 'CPU(\(.+\))', 'MEM(\(.+\))']
 CNT = 0
 
 f = open(LOGFILE,"r")
+tmp_str = ''
+rpi_info_list = []
 
 lines = f.readlines()
 for line in lines:
     if PATTERN.search(line):
         CNT+=1
         rpi_info = re.split(PATTERN, line)
+	#if len(tmp_str) > 0: print "++++++++++\n" + tmp_str + "\n++++++++++"
+	if len(tmp_str) > 0: rpi_info_list.append(tmp_str)
+	tmp_str = ''
     else:
-        #rpi_info.append(line)
         rpi_info = [info for info in rpi_info if len(info) > 0]
-        print rpi_info
+	tmp_str += line
 print CNT
+print rpi_info_list
 
 f.close()
