ffda1d65959a167a3afc6d8bf003fd0159c83932
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-11-08 19:00:20 +0900

    influx db simple application

--- apps/influxdb/influx_simple_driver.py
+++ apps/influxdb/influx_simple_driver.py
@@ -1,22 +1,33 @@
 
+import argparse
+import datetime
 from influxdb import InfluxDBClient
+
+def getTSnow():
+    dtnow = datetime.datetime.now()
+    #print dtnow.strftime('%Y-%m-%dT%H:%M:%S')
+    return dtnow.strftime('%Y-%m-%dT%H:%M:%S')
+
 
 def main(host='localhost', port=8086):
     """Instantiate a connection to the InfluxDB."""
     user = 'root'
     password = 'root'
-    dbname = 'example'
+    dbname = 'kang_test'
+    measurementname = 'sample_data'
     dbuser = 'smly'
     dbuser_password = 'my_secret_password'
     query = 'select value from cpu_load_short;'
+
     json_body = [
         {
-            "measurement": "cpu_load_short",
+            "measurement": measurementname,
             "tags": {
-                "host": "server01",
-                "region": "us-west"
+                "host": "test",
+                "work": "iot_insert"
             },
-            "time": "2009-11-10T23:00:00Z",
+            #"time": "2009-11-10T23:00:00Z",
+            "time": getTSnow(),
             "fields": {
                 "Float_value": 0.64,
                 "Int_value": 3,
@@ -26,30 +37,39 @@ def main(host='localhost', port=8086):
         }
     ]
 
-    client = InfluxDBClient(host, port, user, password, dbname)
+    client = InfluxDBClient(host, port, None, None, dbname)
 
-    print("Create database: " + dbname)
-    client.create_database(dbname)
+    #print("Create database: " + dbname)
+    #client.create_database(dbname)
 
-    print("Create a retention policy")
-    client.create_retention_policy('awesome_policy', '3d', 3, default=True)
+    #print("Create a retention policy")
+    #client.create_retention_policy('awesome_policy', '3d', 3, default=True)
 
-    print("Switch user: " + dbuser)
-    client.switch_user(dbuser, dbuser_password)
+    #print("Switch user: " + dbuser)
+    #client.switch_user(dbuser, dbuser_password)
+
+    #print("Switch user: " + user)
+    #client.switch_user(user, password)
+
+    #print("Drop database: " + dbname)
+    #client.drop_database(dbname)
+
+
+    query = 'show measurements on %s;' %dbname
+    print("Querying data: " + query)
+    result = client.query(query)
+    print("Result: {0}".format(result))
 
     print("Write points: {0}".format(json_body))
     client.write_points(json_body)
 
+    query = 'select * from %s ;' %measurementname
     print("Querying data: " + query)
     result = client.query(query)
-
     print("Result: {0}".format(result))
 
-    print("Switch user: " + user)
-    client.switch_user(user, password)
 
-    print("Drop database: " + dbname)
-    client.drop_database(dbname)
+
 
 def parse_args():
     """Parse the args."""
@@ -64,9 +84,5 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
-    print args
-    exit()
+    #print args
     main(host=args.host, port=args.port)
-
-
-    
