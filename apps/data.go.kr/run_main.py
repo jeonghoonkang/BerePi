ccee6d6ead3988fe2caa9f981c5afe12ccac5bbf
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-11-19 15:36:14 +0900

    Update run_main.py

--- apps/data.go.kr/run_main.py
+++ apps/data.go.kr/run_main.py
@@ -13,7 +13,7 @@ if __name__=="__main__" :
         while True:
             publicdata = get_public_micro_particle.get_public_mise()
             public_val = publicdata[3]
-            ret = influx_simple_driver.influx_write(public_val, 'public_dust', '125.140.110.217')
+            ret = influx_simple_driver.influx_write(public_val, 'public_dust', '127.0.0.1')
                   #def influx_write(val, mname=None, host='127.0.0.1',  port=8086) 
 
             if ret != True : print "[problem|influx]"
