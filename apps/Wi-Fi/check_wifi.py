2ca36698d78c43593456dd88ad062e768ccc7cdb
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-11-23 05:08:33 +0900

    Update check_wifi.py

--- apps/Wi-Fi/check_wifi.py
+++ apps/Wi-Fi/check_wifi.py
@@ -7,6 +7,5 @@ import datetime
 while(1):
     print os.system('sudo sh wifi_cron.sh')
     #print os.system('grep CRON /var/log/syslog')
-    print datetime.datetime.now()
-    
+    print datetime.datetime.now()   
     time.sleep(300)
