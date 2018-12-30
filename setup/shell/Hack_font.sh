962a03d1b2e72ca1e4420a95a0afda74e9a68afc
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2018-12-30 14:12:29 +0900

    Update Hack_font.sh

--- setup/shell/Hack_font.sh
+++ setup/shell/Hack_font.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+#installation for windows, please visit https://github.com/source-foundry/Hack-windows-installer/releases/latest
+
 wget https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip
 
 unzip *.zip
