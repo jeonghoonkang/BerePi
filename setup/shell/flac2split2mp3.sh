12a995af133b9b3733ea61a7d067b9012b141a37
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2019-02-26 01:32:56 +0900

    Update flac2split2mp3.sh

--- setup/shell/flac2split2mp3.sh
+++ setup/shell/flac2split2mp3.sh
@@ -1,4 +1,6 @@
 shnsplit -f *.cue -t %n-%t -o flac *.flac
+# Debian/Ubuntu: sudo apt-get install cuetools shntool
+
 #find ./ -name "*.cue" -print0 | xargs -0 -i -t shnsplit -f {} -t %n-%t -o flac {}.flac
 #find ./ -name "*.cue" -print0 | xargs -0 -i -t dirname {} | xargs -0 -i -t filename {}
 
