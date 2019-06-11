4c79f994121edd78985367f4744f8bafc68ce71a
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2019-06-12 00:10:15 +0900

    Update del_sort_date.sh

--- apps/camera/motion/del_sort_date.sh
+++ apps/camera/motion/del_sort_date.sh
@@ -1,4 +1,7 @@
+#!/bin/baxh
+#Author: github.com/jeonghoonkang
 
 find /var/www/html/cam/motion/ -maxdepth 2  -name '*.avi' -mtime +14 | xargs rm -f
 
 find /var/www/html/cam/motion/ -maxdepth 2  -name '*.jpg' -mtime +14 | xargs rm -f
+
