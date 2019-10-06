e982fb9211624554bece6fc4eb5de339c20e11eb
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2019-10-06 09:43:34 +0900

    ssh key update for input specific file

--- setup/ssh/sshkey.py
+++ setup/ssh/sshkey.py
@@ -8,23 +8,27 @@ import sys
 
 if __name__ == '__main__':
 
-    print ("usage : python %s {ID@IP_ADD} {PORT}, {} : user should input" %__file__)
+    print ("usage : python %s {ID@IP_ADD} {PORT} {file}, {} : user should input" %__file__)
+    print (" if {file} is 'None' : will use /home/user/.ssh/id_rsa ")
 
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 4:
         exit("[bye] you need to input args")
 
     arg1 = sys.argv[1]
     arg2 = sys.argv[2]
+    arg3 = sys.argv[3]
+    if arg3 == 'None':
+        arg3 = '~/.ssh/id_rsa.pub'
     
     ip = arg1
     port = arg2
     print ("... start running", " inputs are ", ip, port)
 
     print ("... key generating")
-    os.system('ssh-keygen')
+    if arg3 == None: os.system('ssh-keygen')
 
     print ("... entering copying security file")
-    run_cmd = "cat ~/.ssh/id_rsa.pub"
+    run_cmd = "cat %s" %(arg3)
     run_cmd += " | ssh -p %s %s" %(port, ip)
     run_cmd += " 'cat>>/home/%s/.ssh/authorized_keys'" %ip[:ip.index('@')]
     print (run_cmd)
