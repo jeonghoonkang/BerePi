49c1fb818b43f6d9518a06bf8ec19c56394db268
Jeonghoon Kang <gadin.kang@gmail.com>
2019-10-09 23:34:57 +0900

    new app dir for do noting

---
+++ apps/null_proc/null_proc.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+import sys
+import time
+
+count = 0
+
+while (True):
+    time.sleep(1)
+    count += 1
+    #print ("hey")
+    #print ( " do nothing well : ", str(count), end="\r")
+    print ( " do nothing well : ", str(count))
+    #print ( " do nothing well \r")
+
+
+
