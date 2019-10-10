3a1d12e29db5e64fe9ff8189904d8c7f5b3d85b0
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2019-10-10 19:18:34 +0900

    Update cam_copy.sh

--- apps/camera/motion/cam_copy.sh
+++ apps/camera/motion/cam_copy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #Author: github.com/jeonghoonkang
+#for RaspberryPi, should use \;
 
 rsync -avhz --partial '--rsh=ssh -p PORTNUM' /var/lib/motion/*.jpg tinyos@IP or URL:webdav/gw/cam
+
+rm /home/pi/cam_data/*.*
+find /var/lib/motion/ -type f -mtime 0 -maxdepth 1 -name '*.jpg' -exec cp '{}' /home/pi/cam_data \;
+rsync -avhz '--rsh=ssh -p2' /home/pi/cam_data/*.jpg ID@IP:webdav/gw/cam/motion
