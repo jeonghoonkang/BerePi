a4e2d30a66549e5f856c63c6cc13a7c2f6093ab2
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2020-03-08 21:34:41 +0900

    Update wav2mp3.sh

--- setup/howto/flac_file/wav2mp3.sh
+++ setup/howto/flac_file/wav2mp3.sh
@@ -5,3 +5,7 @@ for i in *.wav; do ffmpeg -i "$i" -ab 320k "${i%.*}.mp3"; done
 #
 #
 # cuebreakpoints foo.cue | shnsplit -o flac foo.wv -d __PATH__
+
+
+
+# shnsplit -f /home/cuefile.cue -o flac __inputfile__2007.wv__ -t "%p-%a-%n-%t" -d __output_file_
