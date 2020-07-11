2e1427c79c8eb720c4bf0ef4bceb5a4167677c1e
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2020-07-11 11:25:50 +0900

    Update flac2split2mp3.sh

--- apps/flac/flac2split2mp3.sh
+++ apps/flac/flac2split2mp3.sh
@@ -1,6 +1,6 @@
 #find ./ -name "*.cue" -print0 | xargs -0 -i -t shnsplit -f {} -t %n-%t -o flac {}.flac
 
-shnsplit -f *.cue -t %n-%t -o flac *.flac
+shnsplit -f *.cue -t %n_%t_%p_%a -o flac *.flac
 #find ./ -name "*.cue" -print0 | xargs -0 -i -t shnsplit -f {} -t %n-%t -o flac {}.flac
 #find ./ -name "*.cue" -print0 | xargs -0 -i -t dirname {} | xargs -0 -i -t filename {}
 
