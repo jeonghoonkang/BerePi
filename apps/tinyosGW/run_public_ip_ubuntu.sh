1b182955c12da6cc9c6b1113c59f22dab2643e66
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2020-11-14 18:13:26 +0900

    Update run_public_ip_ubuntu.sh

--- apps/tinyosGW/run_public_ip_ubuntu.sh
+++ apps/tinyosGW/run_public_ip_ubuntu.sh
@@ -20,3 +20,6 @@ echo $passwd
 export cmd='python /home/tinyos/devel/BerePi/apps/tinyosGW/publicip.py '$ip' '$port' '$id' '$passwd
 echo $cmd 
 exec $cmd 
+
+# crontab 입력
+# */30 * * * * /bin/bash /home/tinyos/..../run_public_ip_ubuntu.sh
