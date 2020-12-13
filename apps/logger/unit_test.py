7c3cb40c94507fc011d3d30e321f66171344abcc
jeonghoonkang <tinyos@minskangui-iMac.lan>
2020-12-13 14:47:15 +0900

    logger rotate

--- apps/logger/unit_test.py
+++ apps/logger/unit_test.py
@@ -2,11 +2,11 @@
 # author : http://github.com/jeonghoonkang
 
 import sys
-sys.path.append("~/devel/BerePi/apps/logger")
 
+sys.path.append("~/devel/BerePi/apps/logger")
 import berepi_logger
 
 
 if __name__ == "__main__":
-
+    
     berepi_logger.berelog('unit test bere logging', 'success')
