6eeaceb71dfd7a20e230ad7b64e6c6d770f270dc
Jeonghoon Kang <gadin.kang@gmail.com>
2021-02-20 22:22:34 +0900

    logger update

--- apps/logger/berepi_logger.py
+++ apps/logger/berepi_logger.py
@@ -11,16 +11,19 @@ LOG_FILENAME = "/home/pi/devel/BerePi/logs/berelogger.log"
 logger = logging.getLogger('BereLogger')
 logger.setLevel(logging.DEBUG)
 
-handler = logging.handlers.TimedRotatingFileHandler(filename=LOG_FILENAME, when="midnight", interval=1, encoding="utf-8")
-#handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, mode='a', maxBytes=10, backupCount=10)
+# Choose TimeRoatatingFileHandler or RotatingFileHandler 
+#handler = logging.handlers.TimedRotatingFileHandler(filename=LOG_FILENAME, when="midnight", interval=1, encoding="utf-8")
+handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, mode='a', maxBytes=2000, backupCount=1)
 handler.formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 logger.addHandler(handler)
 
 def berelog(msg_name, value):
+    print LOG_FILENAME
     logger.info(msg_name + ' ==> ' + value)
 
 def berelog(msg):
+    print LOG_FILENAME
     logger.info(msg)
 
 if __name__ == "__main__":
@@ -37,3 +40,12 @@ if __name__ == "__main__":
       if you want to use, this berepi_logger
       import logging, and use berelog('*****')
     """
+
+
+'''
+To do:
+    LOG_FILENAME has to have sensor name on the file name
+
+'''
+
+
