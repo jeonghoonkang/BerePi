dcc2b43f117bec5e918f9fbcc0957fad67dad0ca
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2021-05-16 23:23:53 +0900

    Update cam_copy.sh

--- apps/camera/motion/cam_copy.sh
+++ apps/camera/motion/cam_copy.sh
@@ -7,3 +7,7 @@ rsync -avhz --partial '--rsh=ssh -p PORTNUM' /var/lib/motion/*.jpg tinyos@IP or 
 #rm /home/pi/cam_data/*.*
 #find /var/lib/motion/ -type f -mtime 0 -maxdepth 1 -name '*.jpg' -exec cp '{}' /home/pi/cam_data \;
 #rsync -avhz '--rsh=ssh -p2' /home/pi/cam_data/*.jpg ID@IP:webdav/gw/cam/motion
+
+
+#참고
+#  rsync -avhzu --progress -e 'ssh -p 22' id@ip:webdav/bk_office /home/tinyos/work_win
