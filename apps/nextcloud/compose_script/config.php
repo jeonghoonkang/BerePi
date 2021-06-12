b37674cf97f70830d912b5d1cc38e89643274783
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2021-06-12 19:23:28 +0900

    Create config.php

---
+++ apps/nextcloud/compose_script/config.php
@@ -0,0 +1,43 @@
+
+
+
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' =>
+  array (
+    0 =>
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 =>
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'ocmcxciu22zw',
+  'passwordsalt' => '**',
+  'secret' => '**',
+  'trusted_domains' =>
+  array (
+    0 => '***:8888',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '21.0.1.1',
+  'overwrite.cli.url' => 'https://***:8882',
+  'overwriteprotocol' => 'https',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'db',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => '****',
+  'installed' => true,
+);
