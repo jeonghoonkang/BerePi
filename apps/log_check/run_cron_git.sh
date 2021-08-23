8578bebcac82c5e30a5491276eb3a65216dce2e1
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2021-08-24 02:03:14 +0900

    Update run_cron_git.sh

--- apps/log_check/run_cron_git.sh
+++ apps/log_check/run_cron_git.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/tinyos/devel/BerePi
 git pull ~/devel/BerePi
 git commit -m'auto_update' ~/devel/BerePi/apps/log_check/output > ~/devel/BerePi/apps/log_check/error.log 2>&1 
 git push origin master > ~/devel/BerePi/apps/log_check/error.log 2>&1 
