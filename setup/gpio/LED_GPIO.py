6f8ef21ca6928f50c42d8f4444310bb93e362c6d
tinyos <gadin.kang@gmail.com>
2021-08-28 18:30:29 +0900

    add LED_GPIO

--- setup/gpio/LED_GPIO.py
+++ setup/gpio/LED_GPIO.py
@@ -3,7 +3,7 @@ import lgpio
 
 #17,27,22
 
-LED = 17
+LED = 27
 
 # open the gpio chip and set the LED pin as output
 h = lgpio.gpiochip_open(0)
@@ -13,14 +13,25 @@ try:
     while True:
         # Turn the GPIO pin high
         lgpio.gpio_write(h, LED, 1)
+<<<<<<< HEAD
         print ('GIO', LED, 'is Off') #LED 보드 구현에 따라, H->On, L->On 이 다를수 있음
         time.sleep(1)
+=======
+        print ('GIO', LED, 'is On') 
+        time.sleep(3)
+>>>>>>> 76f1ffaf (blink)
 
         # Turn the GPIO pin low
         lgpio.gpio_write(h, LED, 0)
+<<<<<<< HEAD
         print ('GIO', LED, 'is On')
         time.sleep(3)
 
+=======
+        print ('GIO', LED, 'is Off') 
+        time.sleep(1)
+        
+>>>>>>> 76f1ffaf (blink)
 except KeyboardInterrupt:
     lgpio.gpio_write(h, LED, 0)
     lgpio.gpiochip_close(h)
