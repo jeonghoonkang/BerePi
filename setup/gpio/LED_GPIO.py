2d9a8d517ef105f7a2dc4a35165e11023715ee6c
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2021-08-28 14:58:40 +0900

    Update LED_GPIO.py

--- setup/gpio/LED_GPIO.py
+++ setup/gpio/LED_GPIO.py
@@ -1,5 +1,3 @@
-
-
 import time
 import lgpio
 
@@ -13,15 +11,18 @@ lgpio.gpio_claim_output(h, LED)
 
 try:
     while True:
-        # Turn the GPIO pin on
+        # Turn the GPIO pin high
         lgpio.gpio_write(h, LED, 1)
+        print ('GIO', LED, 'is Off') #LED 보드 구현에 따라, H->On, L->On 이 다를수 있음
         time.sleep(1)
 
-        # Turn the GPIO pin off
+        # Turn the GPIO pin low
         lgpio.gpio_write(h, LED, 0)
-        time.sleep(1)
-        
+        print ('GIO', LED, 'is On')
+        time.sleep(3)
+
 except KeyboardInterrupt:
     lgpio.gpio_write(h, LED, 0)
     lgpio.gpiochip_close(h)
-    
+
+
