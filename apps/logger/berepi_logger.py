b2bed9f301f43cd1cdee28814f9a15d05096716f
jeonghoonkang <gadin.kang@gmail.com>
2021-09-18 09:03:41 +0900

    looger simple

--- apps/logger/berepi_logger.py
+++ apps/logger/berepi_logger.py
@@ -6,7 +6,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #logging.config.fileConfig('logging.conf')
-LOG_FILENAME = "/home/pi/devel/BerePi/logs/berelogger.log"
+LOG_FILENAME = "/home/tinyos/devel/BerePi/logs/berelogger.log"
 
 logger = logging.getLogger('BereLogger')
 logger.setLevel(logging.DEBUG)
@@ -18,17 +18,41 @@ handler.formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s',
 
 logger.addHandler(handler)
 
-def berelog(msg_name, value):
-    print LOG_FILENAME
-    logger.info(msg_name + ' ==> ' + value)
+def berelog(msg_name, value=None):
+    print (LOG_FILENAME, 'log file name')
+    if (value == None):
+        logger.info(msg_name )
+    elif (value != None):
+        logger.info(msg_name + ' ==> ' + value)
 
-def berelog(msg):
-    print LOG_FILENAME
-    logger.info(msg)
+def args_proc():
+ 
+    msg = "usage : python %s {} {} {} {}" %__file__
+    msg += " => user should input arguments {} "
+    print (msg, '\n')
+  
+    if len(sys.argv) < 2:
+       exit("[bye] you need to input args, ip / port / id")
+ 
+    arg1 = sys.argv[1]
+    arg2 = sys.argv[2]
+    arg3 = sys.argv[3]
+    arg4 = sys.argv[4]
+ 
+    ip   = arg1
+    port = arg2
+    id   = arg3
+    passwd = arg4
+ 
+    print ("... start running, inputs are ", ip, port, id, passwd)
+ 
+    return ip, port, id, passwd
+
 
 if __name__ == "__main__":
 
-    berelog('sesnor type co2', '25')
+    berelog('temperature', '25')
+    berelog('temperature')
     
     # 'application' code
     #logger.debug('debug message')
@@ -45,7 +69,6 @@ if __name__ == "__main__":
 '''
 To do:
     LOG_FILENAME has to have sensor name on the file name
-
 '''
 
 
