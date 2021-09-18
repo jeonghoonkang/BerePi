f8d29336bfbb5b50acf7c504369f38802ecbaca4
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2021-09-18 15:03:51 +0900

    Update sonno_start.sh

--- system/init_file/sonno_start.sh
+++ system/init_file/sonno_start.sh
@@ -1,10 +1,11 @@
 # should have $SONNO_HOME 
 # this file location $SONNO_HOME/BerePi/system/init_file
 
+# 환경설정을 위한 스크립트
 source /home/tinyos/devel/BerePi/setup/.crontab_env
 
+# 환경변수 없을시, 실행 중지
 set -o nounset
-
 echo "(sonn_check) Please check environment variable SONNO_HOME, it is abolutely to be required " 
 if [[ -v SONNO_HOME ]]
 then
@@ -13,6 +14,7 @@ then
 fi
 echo "SONNO_HOME = "$SONNO_HOME
 
+# 환경변수 존재해야 로그 작성
 python3 $SONNO_HOME/BerePi/apps/logger/berepi_logger.py $1
 
 
