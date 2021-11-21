adad24881614821ad5321767bd95aa1e7149b590
jeonghoonkang <gadin.kang@gmail.com>
2021-11-21 15:19:03 +0900

    merging csv files

--- apps/csv/csv_line_touch.py
+++ apps/csv/csv_line_touch.py
@@ -3,7 +3,96 @@
 
 import text as txt
 import pandas as pd
+import os
+import sys 
+import info #read info for processing
 
+def change_string_to_arry(fstring):
+    sub_buf=[]
+    for idx in range(0,range_end) :
+        line_num += 1
+        print (line_num, firstlist[idx], "<##>", secondlist[idx])
+        #sub_buf.append(line_num)
+        sub_buf.append(firstlist[idx])
+        sub_buf.append(secondlist[idx])
+        buf.append(sub_buf)
+        sub_buf=[]
+    return field_list
+
+def df_rm_header(dfobj, lines=1):
+    #csvobj remove header #lines of line
+    return dfobj 
+
+def recursive_search_dir(_nowDir, _filelist):
+    print(" [loop] recursive searching ", _nowDir)
+
+    if os.path.isfile(_nowDir):
+        file_extension = os.path.splitext(_nowDir)[1]
+        if file_extension == ".csv" or file_extension == ".CSV":
+            _filelist.append(_nowDir)
+        return None
+
+    dir_list = []  # 현재 디렉토리의 서브디렉토리가 담길 list
+    f_list = os.listdir(_nowDir)
+    for fname in f_list:
+        file_extension = os.path.splitext(fname)[1]
+        if os.path.isdir(_nowDir + "/" + fname):
+            dir_list.append(_nowDir + "/" + fname)
+        elif os.path.isfile(_nowDir + "/" + fname):
+            if file_extension == ".csv" or file_extension == ".CSV":
+                _filelist.append(_nowDir + "/" + fname)
+
+    for toDir in dir_list:
+        recursive_search_dir(toDir, _filelist)
+
+def printProgressBar(iteration, total, prefix = 'Progress', suffix = 'Complete',\
+                      decimals = 1, length = 50, fill = '█'): 
+    # 작업의 진행상황을 표시
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' %(prefix, bar, percent, suffix), end='\r')
+    sys.stdout.flush()
+    if iteration == total:
+        print()
+
+def csv_to_df_total(_flist):
+    allData = []# 읽어 들인 csv파일 내용을 저장할 빈 리스트를 하나 만든다
+    cnt = 0
+    print(" 디렉토리 모든 CSV를 데이터프레임으로 변환중 ...")
+    for file in _flist:
+        cnt += 1
+        printProgressBar(cnt, len(_flist))
+        df = pd.read_csv(file, skiprows = 2, header = None) # for구문으로 csv파일들을 읽어 들인다
+        allData.append(df) # 빈 리스트에 읽어 들인 내용을 추가한다
+    
+    dataCombine = pd.concat(allData, axis=0, ignore_index=True)
+    print (cnt, "개의 파일을 처리했습니다.. 아래는 샘플입니다 ")
+    print ("라인스", len(dataCombine.index))
+    dataCombine.sampel(30)
+    return dataCombine
 
 if __name__== "__main__" :
-    None
+    print ("디렉토리 경로", info.local_path)
+    file_list = []
+
+    csvpath = info.local_path
+
+    recursive_search_dir(csvpath, file_list)
+
+    print (file_list)    
+    print (len(file_list))
+    #
+    #csv_to_df_total(file_list)
+
+
+    #df = pd.read_csv ('file_name.csv',usecols= ['column_name1','column_name2']) #subset of fields, sep='\t'
+    #header=0, default, the first row of the CSV file will be treated as column names, header=None
+    #df = pd.read_csv('example.csv', skiprows = 1,header = None)   
+
+
+
+# Ref.
+# https://towardsdatascience.com/how-to-read-csv-file-using-pandas-ab1f5e7e7b58
+# https://www.edureka.co/community/42836/how-to-read-pandas-csv-file-with-no-header
+
---
+++ apps/csv/info.py
@@ -0,0 +1,12 @@
+host='***'
+#port='7776'
+id=''
+passwd=''
+local_path='/Users/tinyos/Downloads/정상CSV'
+#local_path='../../../data_files/MD/'
+#server_path='/home/data/data_original/ev_data/'
+server_path='/home/data/SW-Platform/data_files/EV/'
+todo='download'
+# if you want to carry out directly, 
+# sudo rsync -avhz --progress -e 'ssh -p **' ***@****:/home/data/data_original/ev_data/ ./
+fields_name="timestamp,자동차 속도,브레이크 상태,미션 기어 모드,BCM 미등 상태,BCM 전조등 상태,BCM 왼쪽 방향지시등,BCM 오른쪽 방향지시등,BCM 안전벨트 상태,ECU 배터리 공급 전압,전자식 엑셀 페달 위치,BCM 도어 왼쪽 상태,BCM 도어 오른쪽 상태,BCM 트렁크 상태,EV 배터리 충전 상태,EV BMS 메인릴레이 상태,EV 배터리 사용가능 상태,EV BMS 경고 상태,EV BMS 고장 상태,EV BMS 융착 상태,EV OPD 활성화 상태,EV 동절기 모드 활성화 상태,EV 배터리 팩 전류,EV 배터리 팩 전압,EV 배터리 최대 온도,EV 배터리 최소 온도,EV 배터리 모듈 온도 01,EV 배터리 모듈 온도 02,EV 배터리 모듈 온도 03,EV 배터리 모듈 온도 04,EV 최대 셀 전압,EV 최대 셀 전압 셀 번호,EV 최소 셀 전압,EV 최소 셀 전압 셀 번호,EV 누적 충전 전류량,EV 누적 방전 전류량,EV 누적 충전 전력량,EV 누적 방전 전력량,EV 총 동작시간,EV 절연 저항,EV 최대 충전 파워,EV 최대 방전 파워,EV 배터리 셀 간 전압 편차,EV 급속충전 진행 상태,EV 급속충전 릴레이 상태,EV 완속충전 커넥터 상태,EV 급속충전 커넥터 상태,EV 배터리 SOH,EV 디스플레이,EV 배터리 급속충전 인렛 온도,TPMS 센서(FL) 압력,TPMS 센서(FR) 압력,TPMS 센서(RL) 압력,TPMS 센서(RR) 압력,TPMS 센서(FL) 온도,TPMS 센서(FR) 온도,TPMS 센서(RL) 온도,TPMS 센서(RR) 온도,TPMS 센서(FL) 상태,TPMS 센서(FR) 상태,TPMS 센서(RL) 상태,TPMS 센서(RR) 상태,TPMS 센서(FL) 배터리,TPMS 센서(FR) 배터리,TPMS 센서(RL) 배터리,TPMS 센서(RR) 배터리,ODO 총 운행 거리,EV 베터리 셀 1,EV 베터리 셀 2,EV 베터리 셀 3,EV 베터리 셀 4,EV 베터리 셀 5,EV 베터리 셀 6,EV 베터리 셀 7,EV 베터리 셀 8,EV 베터리 셀 9,EV 베터리 셀 10,EV 베터리 셀 11,EV 베터리 셀 12,EV 베터리 셀 13,EV 베터리 셀 14,EV 베터리 셀 15,EV 베터리 셀 16,EV 베터리 셀 17,EV 베터리 셀 18,EV 베터리 셀 19,EV 베터리 셀 20,EV 베터리 셀 21,EV 베터리 셀 22,EV 베터리 셀 23,EV 베터리 셀 24,EV 베터리 셀 25,EV 베터리 셀 26,EV 베터리 셀 27,EV 베터리 셀 28,EV 베터리 셀 29,EV 베터리 셀 30,EV 베터리 셀 31,EV 베터리 셀 32,EV 베터리 셀 33,EV 베터리 셀 34,EV 베터리 셀 35,EV 베터리 셀 36,EV 베터리 셀 37,EV 베터리 셀 38,EV 베터리 셀 39,EV 베터리 셀 40,EV 베터리 셀 41,EV 베터리 셀 42,EV 베터리 셀 43,EV 베터리 셀 44,EV 베터리 셀 45,EV 베터리 셀 46,EV 베터리 셀 47,EV 베터리 셀 48,EV 베터리 셀 49,EV 베터리 셀 50,EV 베터리 셀 51,EV 베터리 셀 52,EV 베터리 셀 53,EV 베터리 셀 54,EV 베터리 셀 55,EV 베터리 셀 56,EV 베터리 셀 57,EV 베터리 셀 58,EV 베터리 셀 59,EV 베터리 셀 60,EV 베터리 셀 61,EV 베터리 셀 62,EV 베터리 셀 63,EV 베터리 셀 64,EV 베터리 셀 65,EV 베터리 셀 66,EV 베터리 셀 67,EV 베터리 셀 68,EV 베터리 셀 69,EV 베터리 셀 70,EV 베터리 셀 71,EV 베터리 셀 72,EV 베터리 셀 73,EV 베터리 셀 74,EV 베터리 셀 75,EV 베터리 셀 76,EV 베터리 셀 77,EV 베터리 셀 78,EV 베터리 셀 79,EV 베터리 셀 80,EV 베터리 셀 81,EV 베터리 셀 82,EV 베터리 셀 83,EV 베터리 셀 84,EV 베터리 셀 85,EV 베터리 셀 86,EV 베터리 셀 87,EV 베터리 셀 88,EV 베터리 셀 89,EV 베터리 셀 90,EV 베터리 셀 91,EV 베터리 셀 92,EV 베터리 셀 93,EV 베터리 셀 94,EV 베터리 셀 95,EV 베터리 셀 96,EV 베터리 셀 97,EV 베터리 셀 98,EV 배터리 냉각수 인렛 온도,EV 배터리 LTR 후단 온도,위도,경도"
