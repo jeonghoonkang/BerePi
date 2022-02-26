fc2bb5387ecd93b3282af2bfdb312a3ab0ea8e33
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2022-02-26 14:50:21 +0900

    Update co2_elt_mt200.py

--- apps/co2/co2_elt_mt200.py
+++ apps/co2/co2_elt_mt200.py
@@ -1,4 +1,7 @@
 # jeonghoonkang
 
 # http://www.eltsensor.co.kr/products-by-gas/co2/ndir/monitor?tpf=product/view&category_code=101012&code=22
+
+
+if __name__ == '__main__':
 op = serial.openSerial('/dev/ttyUSB0', baudrate=38400, rtscts=True)
