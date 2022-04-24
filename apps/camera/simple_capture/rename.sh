4bbb490cab85e30523387ac2b8e31746f8bb43ef
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2022-04-24 20:52:50 +0900

    Update rename.sh

--- apps/camera/simple_capture/rename.sh
+++ apps/camera/simple_capture/rename.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-for f in *.png; do 
+for f in *.png; do
         echo $f
-        mv -- "$f" "`hostname``date +_%F_%R_`${f}"
+        mv -- "$f" "`hostname``date +_%F_%k%M_`${f}"
         #mv -- "$f" "`date`${f}"
 done
 #$(date +%F_%R)
 #rename -v 's/00000//' *.png
-#mv 00000001.png capture.$(date +%F_%R).jpg                                 
+#mv 00000001.png capture.$(date +%F_%R).jpg
