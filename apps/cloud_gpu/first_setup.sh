3d55358b8591bbb584cbfd4f4f6eb12a107c591c
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2022-05-05 22:08:54 +0900

    Update first_setup.sh

--- apps/cloud_gpu/first_setup.sh
+++ apps/cloud_gpu/first_setup.sh
@@ -4,6 +4,7 @@ apt install htop
 apt install tree
 apt install rsync
 apt install vim
+apt install docker-compose
 
 # rsync 사용법
 # rsync -e 'ssh -p {포트번호}' -avhz -r --progress --partial {아이디}@{주소}:{경로} {다운로드할 경로}
