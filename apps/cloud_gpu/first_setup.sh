cd71337932938054e3500185019672bed683598a
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2022-05-05 21:43:56 +0900

    Update first_setup.sh

--- apps/cloud_gpu/first_setup.sh
+++ apps/cloud_gpu/first_setup.sh
@@ -1,4 +1,8 @@
+# 필요 파일 설치
 apt install screen
 apt install htop
 apt install tree
 apt install rsync
+
+# rsync 사용법
+# rsync -e 'ssh -p {포트번호}' -avhz -r --progress --partial {아이디}@{주소}:{경로} {다운로드할 경로}
