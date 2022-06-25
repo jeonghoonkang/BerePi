46f4dd07d26e8baeff268799b41ba839b62c4424
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2022-06-25 23:00:28 +0900

    Update capture.sh

--- apps/camera/simple_capture/capture.sh
+++ apps/camera/simple_capture/capture.sh
@@ -1,8 +1,33 @@
 rm *.png
 
 sudo mplayer -vo png -frames 3 tv://
+
+readarray -d '' pngarray < <(find . -type f -name '*.png' -print0 )
+
+for f in "${pngarray[@]}";
+do  echo $f; ffmpeg -i $f -vf "drawtext=fontfile=/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf: text='%{localtime}': x=(w-tw)/2: y=h-(2*lh): fontcolor=white: box=1: boxcolor=0x00000000@1: fontsize=30" -r 25 -t 5 "${f%%.png}_text.png";
+done;
 # 3 means number of capture shots
 
-ffmpeg -i 00000001.png -vf "drawtext=fontfile=/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf: text='%{localtime}': x=(w-tw)/2: y=h-(2*lh): fontcolor=white: box=1: boxcolor=0x00000000@1: fontsize=30" -r 25 -t 5 00000001_text.png
-ffmpeg -i 00000002.png -vf "drawtext=fontfile=/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf: text='%{localtime}': x=(w-tw)/2: y=h-(2*lh): fontcolor=white: box=1: boxcolor=0x00000000@1: fontsize=30" -r 25 -t 5 00000002_text.png
-ffmpeg -i 00000003.png -vf "drawtext=fontfile=/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf: text='%{localtime}': x=(w-tw)/2: y=h-(2*lh): fontcolor=white: box=1: boxcolor=0x00000000@1: fontsize=30" -r 25 -t 5 00000003_text.png
+readarray -d '' array < <(find . -type f -name '*_text.png' -print0 )
+
+dt=$(date "+%Y_%m%d-%H%M_%S")
+for i in "${array[@]}";
+do ii=${i##*/} && ii=${ii%%_text.png} && echo $ii && echo mv "$i" "u-$ii-$dt.png" && mv "$i" "u-$ii-$dt.png";
+done;
+
+
+
+#for i do
+#    d=$(dirname "$i")
+#    [ "$d" = / ] && d=
+
+#ffmpeg -i 00000003.png -vf "drawtext=fontfile=/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf: text='%{localtime}': x=(w-tw)/2: y=h-(2*lh): fontcolor=white: box=1: boxcolor=0x00000000@1: fontsize=30" -r 25 -t 5 00000003_text.png
+
+#readarray -d '' array < <(find . -type f -name '*.png' -print0 ) && for i in "${array[@]}"; do echo $i; done;
+
+#${string##substring} Deletes longest match of $substring from front of $string.
+#${string%%substring} Deletes longest match of $substring from back of $string.
+#disk="/dev/sda"
+#local dev_node=${disk##*/}
+#substring matching */ from /dev/sda results in sda
