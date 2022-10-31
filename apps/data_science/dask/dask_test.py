652bd047ec213c31a6b15a375b1d9c50cff05d92
tinyos <tinyos@iMac-i5-kang.local>
2022-10-31 22:30:15 +0900

    dask install

--- apps/data_science/dask/dask_test.py
+++ apps/data_science/dask/dask_test.py
@@ -4,3 +4,31 @@ from prophet import Prophet
 df = pd.read_csv('https://raw.githubusercontent.com/facebook/prophet/master/examples/example_wp_log_peyton_manning.csv')
 df['y'] = df['y'].apply(lambda x: x + 1)
 print (df.head())
+
+import numpy as np
+
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource, RangeTool
+from bokeh.plotting import figure, show
+from bokeh.sampledata.stocks import AAPL
+
+dates = np.array(AAPL['date'], dtype=np.datetime64)
+source = ColumnDataSource(data=dict(date=dates, close=AAPL['close']))
+
+p = figure(height=300, width=800, tools="xpan", toolbar_location=None, x_axis_type="datetime", x_axis_location="above", background_fill_color="#efefef", x_range=(dates[1500], dates[2500]))
+
+p.line('date', 'close', source=source)
+p.yaxis.axis_label = 'Price'
+
+select = figure(title="Drag the middle and edges of the selection box to change the range above", height=130, width=800, y_range=p.y_range, x_axis_type="datetime", y_axis_type=None, tools="", toolbar_location=None, background_fill_color="#efefef")
+
+range_tool = RangeTool(x_range=p.x_range)
+range_tool.overlay.fill_color = "navy"
+range_tool.overlay.fill_alpha = 0.2
+
+select.line('date', 'close', source=source)
+select.ygrid.grid_line_color = None
+select.add_tools(range_tool)
+select.toolbar.active_multi = range_tool
+
+show(column(p, select))
