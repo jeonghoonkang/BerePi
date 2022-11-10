553751c2a19b6d9e1739110003a0d38c79361770
jeonghoonkang <gadin.kang@gmail.com>
2022-11-11 07:51:45 +0900

    mysql sample

--- apps/db/mysql_test.py
+++ apps/db/mysql_test.py
@@ -22,4 +22,49 @@ if __name__=="__main__":
 
 
 
+'''
+# Routine to insert temperature records into the pidata.temps table:
+def insert_record( device, datetime, temp, hum ):
+	query = "INSERT INTO temps3 (device,datetime,temp,hum) VALUES (%s,%s,%s,%s)"
+    	args = (device,datetime,temp,hum)
+
+    	try:
+        	conn = MySQLdb.connect( host=hostname, user=username, passwd=password, db=database )
+		cursor = conn.cursor()
+        	cursor.execute(query, args)
+		conn.commit()
+
+    	except Exception as error:
+        	print(error)
+
+    	finally:
+        	cursor.close()
+        	conn.close()
+
+# Print welcome 
+print('[{0:s}] starting on {1:s}...'.format(prog_name, datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')))
+
+# Main loop
+try:
+	while True:
+		hum, temp = Adafruit_DHT.read_retry(dht_sensor_type, dht_sensor_port)
+		temp = temp * 9/5.0 + 32
+		now = datetime.datetime.now()
+		date = now.strftime('%Y-%m-%d %H:%M:%S')
+		insert_record(device,str(date),format(temp,'.2f'),format(hum,'.2f'))
+		time.sleep(180)
+		
+except (IOError,TypeError) as e:
+	print("Exiting...")
+
+except KeyboardInterrupt:  
+    	# here you put any code you want to run before the program   
+    	# exits when you press CTRL+C  
+	print("Stopping...")
+
+finally:
+	print("Cleaning up...")  
+	GPIO.cleanup() # this ensures a clean exit
+'''
+
 
