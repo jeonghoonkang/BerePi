051ce7ad803e9b41b41b8b01d11836ab71e6c45f
jeonghoonkang <gadin.kang@gmail.com>
2022-12-11 08:55:30 +0900

    voice for ppt

---
+++ apps/pptvoice/test_pptvoice.py
@@ -0,0 +1,41 @@
+# export GOOGLE_APPLICATION_CREDENTIALS="/Users/****.json"
+
+# from code 
+# https://github.com/GoogleCloudPlatform/python-docs-samples/blob/main/texttospeech/snippets/quickstart.py
+
+"""Synthesizes speech from the input string of text or ssml.
+Make sure to be working in a virtual environment.
+
+Note: ssml must be well-formed according to:
+    https://www.w3.org/TR/speech-synthesis/
+"""
+from google.cloud import texttospeech
+
+# Instantiates a client
+client = texttospeech.TextToSpeechClient()
+
+# Set the text input to be synthesized
+synthesis_input = texttospeech.SynthesisInput(text="Hello, Have good day, good music, good world cup")
+
+# Build the voice request, select the language code ("en-US") and the ssml
+# voice gender ("neutral")
+voice = texttospeech.VoiceSelectionParams(
+    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+)
+
+# Select the type of audio file you want returned
+audio_config = texttospeech.AudioConfig(
+    audio_encoding=texttospeech.AudioEncoding.MP3
+)
+
+# Perform the text-to-speech request on the text input with the selected
+# voice parameters and audio file type
+response = client.synthesize_speech(
+    input=synthesis_input, voice=voice, audio_config=audio_config
+)
+
+# The response's audio_content is binary.
+with open("output.mp3", "wb") as out:
+    # Write the response to the output file.
+    out.write(response.audio_content)
+    print('Audio content written to file "output.mp3"')
