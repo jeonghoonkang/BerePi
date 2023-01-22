ec8106156e591764b1347a9f4ace81a30b7652fc
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2023-01-22 14:22:40 +0900

    Update this_run_public_ip_rpi.sh

--- apps/tinyosGW/this_run_public_ip_rpi.sh
+++ apps/tinyosGW/this_run_public_ip_rpi.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 # Author : jeonghoonkang, https://github.com/jeonghoonkang
 
-export ip=''
-export port=''
-export id=''
-export passwd=''
+echo " Plaase check the PATH to this file"
+echo " Run argument {0} =>  "${0}
+this_path=${0}
+echo " using this path => "$this_path
+#echo " Input Argument "${0##*/}
+
+head_path='/home/tinyos/devel_opment'
+tail_path='/BerePi/apps/tinyosGW'
+run_cmd='python '$head_path$tail_path' this_run_public_rpi.sh'
+
+#export ip=''
+#export port=''
+#export id=''
+#export passwd=''
 export ip=$1
 export port=$2
 export id=$3
 export passwd=$4
 
-echo $ip
-echo $port
-echo $id
-echo $passwd
+echo $ip $port $id $passwd
 
-#echo " PATH to me "${0}
-#echo " FILENAME "${0##*/}
+export cmd='python '$head_path$tail_path'/publicip.py '$ip' '$port' '$id' '$passwd
+echo ' now run it => '$cmd 
+$cmd 
 
-#export cmd='python /home/pi/devel/BerePi/apps/tinyosGW/publicip.py {ip or URL} {port} {ID} {password}'
-export cmd='python /home/pi/devel/BerePi/apps/tinyosGW/publicip.py '$ip' '$port' '$id' '$passwd
-echo '[script file:] '${0##*/}
-echo $cmd 
-exec $cmd 
+# export cmd='python /home/pi/devel/BerePi/apps/tinyosGW/publicip.py {ip or URL} {port} {ID} {password}'
