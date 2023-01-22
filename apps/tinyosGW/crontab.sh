c70a1f995bae4f88abc0212aa175286adc6b715b
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2023-01-22 14:23:35 +0900

    Update crontab.sh

--- apps/tinyosGW/crontab.sh
+++ apps/tinyosGW/crontab.sh
@@ -1,1 +1,2 @@
-*/45 * * * * sh /home/pi/devel/BerePi/apps/tinyosGW/this_run_public_ip.sh > /home/pi/logs/crontab_debug.log 2>&1
+# */45 * * * * sh /home/pi/devel/BerePi/apps/tinyosGW/this_run_public_ip.sh > /home/pi/logs/crontab_debug.log 2>&1
+*/45 * * * * sh /home/tinyos/devel_opment/BerePi/apps/tinyosGW/this_run_public_ip.sh > /home/tinyos/logs/crontab_debug.log 2>&1
