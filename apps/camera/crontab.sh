dc669c8ffd54fd7b32d83ceaecc3dae0b1be83a5
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2023-01-24 14:40:13 +0900

    Update crontab.sh

--- apps/camera/crontab.sh
+++ apps/camera/crontab.sh
@@ -1,5 +1,5 @@
 
 
-*/20 * * * * fswebcam -r 1280*960 /home/tinyos/web/png/image_$(date '+%Y-%m-%d').jpg > /home/tinyos/devel_opment/log/crontab.camera.log 2>&1
+*/20 * * * * fswebcam -r 1280*960 /home/tinyos/web/png/image_$(date '+%Y-%m-%d_%H:%M:%S').jpg > /home/tinyos/devel_opment/log/crontab.camera.log 2>&1
 
-#fswebcam -r 1280*960 /home/tinyos/web/png/image_$(date '+%Y-%m-%d').jpg
+#fswebcam -r 1280*960 /home/tinyos/web/png/image_$(date '+%Y-%m-%d_%H:%M:%S').jpg
