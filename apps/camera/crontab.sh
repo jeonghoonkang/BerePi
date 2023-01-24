426479f971e8d29005e176278b68e1849f1ec50d
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2023-01-24 17:52:20 +0900

    Update crontab.sh

--- apps/camera/crontab.sh
+++ apps/camera/crontab.sh
@@ -2,7 +2,9 @@
 
 */30 * * * * bash /home/tinyos/cam.sh > /home/tinyos/devel_opment/log/crontab.camera.log 2>&1
 
-#fswebcam -r 1280*960 /home/tinyos/web/png/image_$(date '+%Y-%m-%d_%H:%M:%S').jpg
+# 아래 실행 명령은 crontab 에서는 동작하지 않음
+# *.sh 파일로 저장하고 실행해야 함, 위 처럼, bash /home/tinyos/cam.sh 로 실행 
+# fswebcam -r 1280*960 /home/tinyos/web/png/image_$(date '+%Y-%m-%d_%H:%M:%S').jpg
 
 
 # cam.sh
