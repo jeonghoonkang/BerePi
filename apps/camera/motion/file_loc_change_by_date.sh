3e7d59ee69117876c56d739a132cd42939d1fa7a
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2023-01-25 15:53:09 +0900

    Update file_loc_change_by_date.sh

--- apps/camera/motion/file_loc_change_by_date.sh
+++ apps/camera/motion/file_loc_change_by_date.sh
@@ -13,3 +13,37 @@ for f in /volume1/video/*; do
   echo mv "$f" "$target"
 done
 
+
+
+
+#!/bin/bash
+
+SOURCE_FOLDER="video/"
+TARGET_FOLDER="video/daily" # Not used as it is under the source folder.
+
+doMove() {
+    # Year loop assuming AFTER 2000 up to 2022
+    cd "$SOURCE_FOLDER"
+    for i in 20{19..20} ; do
+        #echo -e "/nWorking on 20$1"
+        # Month Loop
+        for j in 0{1..9} {10..12} ; do
+            # Day Loop
+            for k in 0{1..9} {10..31} ; do
+                # Find files matching YYMMDD
+                file=$(find . -maxdepth 1 -type f -print | grep $i$j$k)
+                # For each found file:
+                for sf in $file ; do
+                    # Find the folder with a different format
+                    folder=$(find * -type d -print | grep $i-$j-$k)
+                    echo -e "\nFile: $sf"
+                    echo -e "Folder: $folder"
+                    mv "$sf" "$folder/" # 2> /dev/null
+                    echo -e "Moved $sf to $folder"
+                done
+            done
+        done
+    done
+    cd .. # Change to your needs.
+}
+doMove
