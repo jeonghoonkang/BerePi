3e3894e75704192e40ea5d33a9a71cd23a142396
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2023-02-18 20:08:09 +0900

    Update crontab.sh

--- apps/camera/crontab.sh
+++ apps/camera/crontab.sh
@@ -1,14 +1,19 @@
 
-
-*/30 * * * * bash /home/tinyos/cam.sh > /home/tinyos/devel_opment/log/crontab.camera.log 2>&1
+*/30 * * * * bash /home/cam.sh > /home/.../.../log/crontab.camera.log 2>&1
 
 # 아래 실행 명령은 crontab 에서는 동작하지 않음
 # *.sh 파일로 저장하고 실행해야 함, 위 처럼, bash /home/tinyos/cam.sh 로 실행 
 # fswebcam -r 1280*960 /home/tinyos/web/png/image_$(date '+%Y-%m-%d_%H:%M:%S').jpg
 
-
 # cam.sh
+#
+#dest='{SRC}'
+#
 # fswebcam -r 1280*960 /home/tinyos/web/png/image_$(date '+%Y-%m-%d_%H:%M:%S').jpg
 # 
 # @hourly echo "password" | sudo -S rm somefile
+# sudo chowm www-data -R /home/.../web/png
+# sudo find /home/.../web/png -name image_* -mtime +14 -delete
+# sudo rsync -avhz --partial --progress /home/.../web/png $dest
+# sudo docker exec -it -u 33 ..._app_1 php occ files:scan --all
 #
