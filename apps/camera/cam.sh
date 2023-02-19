647c2e11f3166b1be2a97c075b640fc4456a283d
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2023-02-20 00:24:29 +0900

    Update cam.sh

--- apps/camera/cam.sh
+++ apps/camera/cam.sh
@@ -1,6 +1,6 @@
+
 dest=/home/~~~/path
 fswebcam -r 1280*960 /home/{SRC_DIR}/image_$(date '+%Y-%m-%d_%H:%M:%S').jpg
 sudo rsync -avhz --partial --progress {SRC} $dest
-sudo docker exec -it -u 33 script_95_proxy_1 php occ files:scan --all
-
+sudo docker exec -i -u 33 app_1 php occ files:scan --all
 
