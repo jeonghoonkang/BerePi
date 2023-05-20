913456de10a6994f96f1a278f753071cdb5cb5be
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2023-05-20 12:29:59 +0900

    Update sysbench_run.sh

--- setup/howto/sysbench_run.sh
+++ setup/howto/sysbench_run.sh
@@ -6,7 +6,7 @@ when=$START
 while true; do $order; echo ""; $s1; echo ""; $s2; 
 echo "---------- split line ----------  (info)"$PERIOD" secs peroid  "; 
 echo " first-shot run time:"; echo $when; echo " ----loop end---- "; 
-echo " Raspi 3 - "
+echo " Raspi 3 - CPU speed events per second:  22.71"
 echo " Raspi 4 - CPU speed events per second:  1749.76 "
 
 sleep $PERIOD;
