3e317a371f5c02df785ff3dcdda04c67ddc1f523
ipmstyle <ipmstyle@gmail.com>
2023-06-23 12:26:01 +0100

    add compare.py

--- apps/machine_learning/whisper/test-whisper.py
+++ apps/machine_learning/whisper/test-whisper.py
@@ -5,10 +5,12 @@ startTime = time.time()
 if len(sys.argv) == 2:
     os.system("whisper %s --language=Korean" % sys.argv[1] )
     endTime = time.time()
+    print("file : %s" % sys.argv[1])
     print("processing time : %s" % (endTime - startTime))
 elif len(sys.argv) > 1:
     os.system("whisper %s --model=%s --language=Korean --output_dir=%s" % (sys.argv[1], sys.argv[2], sys.argv[2]) )
     endTime = time.time()
+    print("file : %s" % sys.argv[1])
     print("processing time : %s" % (endTime - startTime))
 else:
     print("require a mp3 file")
