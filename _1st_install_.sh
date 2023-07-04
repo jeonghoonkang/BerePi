a26cfbd739ca399878bd38b778223081811ee6bf
jeonghoonkang <gadin.kang@gmail.com>
2023-07-05 02:20:10 +0900

    1st setup

--- _1st_install_.sh
+++ _1st_install_.sh
@@ -2,4 +2,4 @@
 echo ".... Download script file ...."
 wget https://raw.githubusercontent.com/jeonghoonkang/BerePi/master/setup/init.sh
 # to start setup
-# for the Ubuntu (PC)
+# for the Ubuntu (PC and RaspberryPi)
