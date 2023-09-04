6124b1cbbd59afc96f5f7725a5b4c5d6d4e4a93c
jeonghoonkang <gadin.kang@gmail.com>
2023-09-05 04:36:59 +0900

    test ocr

--- apps/camera/ocr/tesseract_test.py
+++ apps/camera/ocr/tesseract_test.py
@@ -1,4 +1,18 @@
 
 import pytesseract
 import cv2
+# pip3 install opencv-python
+# print(cv2.getVersionString())
+
 ### ~/devel/data/ocr/sample_receipt.png 
+import matplotlib
+import os
+
+image = cv2.imread(os.path.expanduser('~/devel/data/ocr/sample_receipt.png'))
+rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+text = pytesseract.image_to_string(rgb_image)
+
+
+
+## info for Korean OCR
+### https://github.com/yunwoong7/korean_ocr_using_pororo
