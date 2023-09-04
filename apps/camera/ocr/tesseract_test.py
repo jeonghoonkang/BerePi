62605fae5ff5e5440da1a6ee0e76aa147bd575b5
jeonghoonkang <gadin.kang@gmail.com>
2023-09-05 04:52:34 +0900

    test ocr

--- apps/camera/ocr/tesseract_test.py
+++ apps/camera/ocr/tesseract_test.py
@@ -1,5 +1,9 @@
 
 import pytesseract
+# sudo apt update
+# sudo apt install tesseract-ocr
+# sudo apt install libtesseract-dev
+
 import cv2
 # pip3 install opencv-python
 # print(cv2.getVersionString())
@@ -12,7 +16,12 @@ image = cv2.imread(os.path.expanduser('~/devel/data/ocr/sample_receipt.png'))
 rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 text = pytesseract.image_to_string(rgb_image)
 
+print (text)
 
 
 ## info for Korean OCR
 ### https://github.com/yunwoong7/korean_ocr_using_pororo
+
+
+## try out the following
+### https://weejw.tistory.com/611
