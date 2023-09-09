27504a60c145238e8dbb7a2c743fd7c7fc5dfa21
Jeonghoon Kang <gadin.kang@gmail.com>
2023-09-09 14:45:16 +0900

    easyocr

--- apps/camera/ocr/easyocr_run.py
+++ apps/camera/ocr/easyocr_run.py
@@ -3,12 +3,38 @@
 
 import easyocr
 from pprint import pprint
+import os, sys
+import argparse
 
-fpath='/home/tinyos/devel_opment/data/ocr/sample_receipt.png'
+argparser = argparse.ArgumentParser()
+argparser.add_argument('-lang', '--language', help='language, ko, ja', default='ko')
+argparser.add_argument('-f', '--file', help='file name', default='./sample.jpg')
+args = argparser.parse_args()
 
-#(os.path.expanduser('~/devel/data/ocr/sample_receipt.png'))
+file_name = args.file
+language = args.language
+print (file_name, language)
 
-reader = easyocr.Reader(['ko','en'])
+reader = easyocr.Reader(['en',language]) #language is changing string
+
+chk_string = ["ko","ja"]
+for chk in chk_string:
+    print (chk)
+
+if (not any( chk in language for chk in chk_string)): # if language is not in chk_string:
+    print (language)
+    print ("language is not in ko, ja")
+    sys.exit("language mismatch")
+    
+print (file_name, language)
+fpath = file_name
+
+#fpath='/home/tinyos/devel_opment/data/ocr/sample_receipt.png'
+
+reader = easyocr.Reader([language,'en']) #language
+
+print (sys.argv)
+
 result = reader.readtext(fpath)
 
 pprint(result, depth=2, indent=4)
