82848d414468c0079b74ffcfea799ce5bc2eb1bf
jeonghoonkang <gadin.kang@gmail.com>
2024-03-13 00:19:36 +0900

    scan file

--- apps/filesystem/scan_new_file.py
+++ apps/filesystem/scan_new_file.py
@@ -4,22 +4,32 @@ import threading
 import glob
 import os
 
-f1 = glob.glob('*.mkv')
+FILE_TYPE = "*.mkv"
+Thread_time = 10.0
+
+f1 = glob.glob(FILE_TYPE)
 
 def status():
-
-    threading.Timer(5.0, status).start()
-    f2 = glob.glob('*.mkv')
+    
+    threading.Timer(Thread_time, status).start()
+    f2 = glob.glob(FILE_TYPE)
     f3 = set(f2) - set(f1)
 
     if len(f3) == 0 :
-        print ("No new file")
+        pass
+        #print ("No new file")
     else:
+        print (os.getcwd())        
         print ("got new file trigger")
         print (list(f3)[0])
-        print ("list element")
-        print (list(f3)) 
+        #print ("list element")
+        #print (list(f3)) 
 
-print (os.getcwd())
 
-status()
+
+if __name__ == "__main__":
+    print ("scan_new_file.py is being run directly")
+    print (os.getcwd())
+    print ("scanning new file of ", FILE_TYPE, "Thread time: ", Thread_time )
+    #run scan 
+    status()
