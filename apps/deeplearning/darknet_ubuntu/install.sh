7ec8bac1dffdd26d4aad377ecb77a7e6a94f00b5
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2024-08-08 02:36:50 +0900

    Update install.sh

--- apps/deeplearning/darknet_ubuntu/install.sh
+++ apps/deeplearning/darknet_ubuntu/install.sh
@@ -1,3 +1,13 @@
 git clone https://github.com/pjreddie/darknet.git
 cd darknet
 
+# before make, you should change Make file for using GPU
+# GPU=1 
+# CUDNN=1 
+# CUDNN_HALF=0 
+# OPENCV=1 
+# AVX=0 
+# OPENMP=0 
+# LIBSO=1
+
+# and make run 
