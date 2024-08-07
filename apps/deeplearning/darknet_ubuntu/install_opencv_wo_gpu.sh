fd843e081f4685cf729ebf84ff95a59311417e77
jeonghoonkang <gadin.kang@gmail.com>
2024-08-08 02:12:24 +0900

    fix install darknet

---
+++ apps/deeplearning/darknet_ubuntu/install_opencv_wo_gpu.sh
@@ -0,0 +1,24 @@
+sudo apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt install python3-dev python3-numpy
+git clone https://github.com/opencv/opencv.git
+cd opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D BUILD_EXAMPLES=ON \
+      -D WITH_CUDA=OFF \
+      -D WITH_OPENCL=OFF \
+      -D WITH_OPENGL=OFF ..
+make -j$(nproc)
+sudo make install
+sudo ldconfig
+
+
+
+# MAC OSX
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# brew install opencv
+# import cv2
+# print(cv2.__version__)
+
