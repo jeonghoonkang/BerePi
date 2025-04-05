406de1e43e1472f4d8121405c5eb4de58d0d42aa
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2025-04-06 06:17:54 +0900

    Update chk_url.sh

--- chk_url.sh
+++ chk_url.sh
@@ -1,1 +1,4 @@
+# Author : https://github.com/jeonghoonkang
+# DNS URL expire check
+# 도메인 기간 SSL 
 URL="****.com"; FURL=$URL":8881"; openssl s_client -connect $FURL -servername $URL 2>/dev/null | openssl x509 -noout -dates
