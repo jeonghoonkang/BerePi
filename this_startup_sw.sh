9c1487636027212e5193fd1b95895c7ef9ca7e31
jeonghoonkang <gadin.kang@gmail.com>
2025-05-10 08:14:16 +0900

    fix

--- this_startup_sw.sh
+++ this_startup_sw.sh
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # Author : https://github.com/jeonghoonkang
 
+
 alias sr='screen'
 #sudo screen -dmS startup sudo bash /home/pi/devel/BerePi/apps/otsdb/start_tsdb.sh
 #sudo screen -dmS lcd sudo python /home/pi/devel/BerePi/apps/lcd_berepi/watch.py -ip xxxxx
