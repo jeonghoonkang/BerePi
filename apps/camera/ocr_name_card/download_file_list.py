753e7ac4fd821378377ec48f6857978c2579ee4e
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2025-08-21 00:36:42 +0900

    feat(camera): add webdav downloader

---
+++ apps/camera/ocr_name_card/download_file_list.py
@@ -0,0 +1,27 @@
+import json
+from webdav3.client import Client
+
+
+def download_file_list(config_path: str = "nocommit_url2.ini") -> None:
+    """Read WebDAV credentials from a JSON config and download file_list.json."""
+    with open(config_path, 'r', encoding='utf-8') as f:
+        config = json.load(f)
+
+    nc_cfg = config.get('nextcloud', {})
+    options = {
+        'webdav_hostname': nc_cfg.get('webdav_hostname'),
+        'webdav_login': nc_cfg.get('username'),
+        'webdav_password': nc_cfg.get('password'),
+    }
+
+    client = Client(options)
+    client.verify = True
+
+    remote_path = '/Photos/biz_card/file_list.json'
+    local_path = './file_list.json'
+
+    client.download_sync(remote_path=remote_path, local_path=local_path)
+
+
+if __name__ == '__main__':
+    download_file_list()
